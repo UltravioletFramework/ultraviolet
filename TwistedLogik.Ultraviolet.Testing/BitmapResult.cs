cd33847cc40c310ca0168927847b2d9105a7f928
Cole Campbell <webmaster@twistedlogik.net>
2014-10-31 14:21:20 -0400

    Added tolerance to BitmapResult

--- TwistedLogik.Ultraviolet.Testing/BitmapResult.cs
+++ TwistedLogik.Ultraviolet.Testing/BitmapResult.cs
@@ -19,6 +19,17 @@ namespace TwistedLogik.Ultraviolet.Testing
         }
 
         /// <summary>
+        /// Specifies that subsequent comparisons should be within the specified linear distance.
+        /// </summary>
+        /// <param name="distance">The distance value to set.</param>
+        /// <returns>The result object.</returns>
+        public BitmapResult WithinDistance(Single distance)
+        {
+            this.distance = distance;
+            return this;
+        }
+
+        /// <summary>
         /// Asserts that the bitmap matches the image in the specified file.
         /// </summary>
         /// <param name="filename">The filename of the image to match against the bitmap.</param>
@@ -36,7 +47,7 @@ namespace TwistedLogik.Ultraviolet.Testing
                 {
                     var pixelExpected = expected.GetPixel(x, y);
                     var pixelActual   = bitmap.GetPixel(x, y);
-                    if (pixelExpected != pixelActual)
+                    if (CalculateColorDistance(pixelExpected, pixelActual) > distance)
                     {
                         Assert.Fail("Images do not match");
                     }
@@ -52,7 +63,24 @@ namespace TwistedLogik.Ultraviolet.Testing
             get { return bitmap; }
         }
 
+        /// <summary>
+        /// Calculates the linear distance between two colors.
+        /// </summary>
+        /// <param name="c1">The first <see cref="Color"/> to compare.</param>
+        /// <param name="c2">The second <see cref="Color"/> to compare.</param>
+        /// <returns>The linear distance between the specified color.</returns>
+        private static Single CalculateColorDistance(System.Drawing.Color c1, System.Drawing.Color c2)
+        {
+            return (Single)Math.Sqrt(
+                Math.Pow(c1.A - c2.A, 2) +
+                Math.Pow(c1.R - c2.R, 2) +
+                Math.Pow(c1.G - c2.G, 2) +
+                Math.Pow(c1.B - c2.B, 2)
+            );
+        }
+
         // State values.
         private readonly Bitmap bitmap;
+        private Single distance = 5.0f;
     }
 }
