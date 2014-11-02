d5798e00c62d3e1c780148347253ee0f7f9c51d5
Cole Campbell <webmaster@twistedlogik.net>
2014-11-02 15:14:17 -0500

    XML documentation fixes

--- TwistedLogik.Ultraviolet/Graphics/Graphics2D/NamespaceDoc.cs
+++ TwistedLogik.Ultraviolet/Graphics/Graphics2D/NamespaceDoc.cs
@@ -3,7 +3,7 @@
 namespace TwistedLogik.Ultraviolet.Graphics.Graphics2D
 {
     /// <summary>
-    /// The TwistedLogik.Ultraviolet.Graphics.Graphics2D namespace contains types used for rendering 2D graphics.
+    /// The <see cref="TwistedLogik.Ultraviolet.Graphics.Graphics2D"/> namespace contains types used for rendering 2D graphics.
     /// </summary>
     [CompilerGenerated]
     class NamespaceDoc
--- TwistedLogik.Ultraviolet/Graphics/Graphics2D/Text/NamespaceDoc.cs
+++ TwistedLogik.Ultraviolet/Graphics/Graphics2D/Text/NamespaceDoc.cs
@@ -3,7 +3,7 @@
 namespace TwistedLogik.Ultraviolet.Graphics.Graphics2D.Text
 {
     /// <summary>
-    /// The TwistedLogik.Ultraviolet.Graphics.Graphics2D.Text namespace contains types used for rendering formatted strings of text.
+    /// The <see cref="TwistedLogik.Ultraviolet.Graphics.Graphics2D.Text"/> namespace contains types used for rendering formatted strings of text.
     /// </summary>
     [CompilerGenerated]
     class NamespaceDoc
