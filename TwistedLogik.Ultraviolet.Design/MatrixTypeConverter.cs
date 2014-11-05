2199e7f98c1e6cc9ac7fa2dddaea800d2253af92
tlgkccampbell <webmaster@twistedlogik.net>
2014-11-05 15:09:13 -0500

    Added basic type converters for Vector2, Vector3, Vector4, Matrix, Color

---
+++ TwistedLogik.Ultraviolet.Design/MatrixTypeConverter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+
+namespace TwistedLogik.Ultraviolet.Design
+{
+    /// <summary>
+    /// Represents a type converter for the <see cref="Matrix"/> structure.
+    /// </summary>
+    public class MatrixTypeConverter : UltravioletExpandableObjectConverter<Matrix>
+    {
+        /// <inheritdoc/>
+        public override Boolean GetCreateInstanceSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        /// <inheritdoc/>
+        public override Object CreateInstance(ITypeDescriptorContext context, IDictionary propertyValues)
+        {
+            var m11 = (Single)propertyValues["M11"];
+            var m12 = (Single)propertyValues["M12"];
+            var m13 = (Single)propertyValues["M13"];
+            var m14 = (Single)propertyValues["M14"];
+
+            var m21 = (Single)propertyValues["M21"];
+            var m22 = (Single)propertyValues["M22"];
+            var m23 = (Single)propertyValues["M23"];
+            var m24 = (Single)propertyValues["M24"];
+
+            var m31 = (Single)propertyValues["M31"];
+            var m32 = (Single)propertyValues["M32"];
+            var m33 = (Single)propertyValues["M33"];
+            var m34 = (Single)propertyValues["M34"];
+
+            var m41 = (Single)propertyValues["M41"];
+            var m42 = (Single)propertyValues["M42"];
+            var m43 = (Single)propertyValues["M43"];
+            var m44 = (Single)propertyValues["M44"];
+
+            return new Matrix(
+                m11, m12, m13, m14,
+                m21, m22, m23, m24,
+                m31, m32, m33, m34,
+                m41, m42, m43, m44
+            );
+        }
+
+        /// <inheritdoc/>
+        protected override String GetStringRepresentation(Object value)
+        {
+            var matrix = (Matrix)value;
+            return String.Format("({0}, {1}, {2}, {3}), ({4}, {5}, {6}, {7}), ({8}, {9}, {10}, {11}), ({12}, {13}, {14}, {15})", 
+                matrix.M11, matrix.M12, matrix.M13, matrix.M14,
+                matrix.M21, matrix.M22, matrix.M23, matrix.M24,
+                matrix.M31, matrix.M32, matrix.M33, matrix.M34,
+                matrix.M41, matrix.M42, matrix.M43, matrix.M44                
+            );
+        }
+    }
+}
---
+++ TwistedLogik.Ultraviolet.Design/Vector4TypeConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+
+namespace TwistedLogik.Ultraviolet.Design
+{
+    /// <summary>
+    /// Represents a type converter for the <see cref="Vector4"/> structure.
+    /// </summary>
+    public class Vector4TypeConverter : UltravioletExpandableObjectConverter<Vector4>
+    {
+        /// <inheritdoc/>
+        public override Boolean GetCreateInstanceSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        /// <inheritdoc/>
+        public override Object CreateInstance(ITypeDescriptorContext context, IDictionary propertyValues)
+        {
+            var x = (Single)propertyValues["X"];
+            var y = (Single)propertyValues["Y"];
+            var z = (Single)propertyValues["Z"];
+            var w = (Single)propertyValues["W"];
+
+            return new Vector4(x, y, z, w);
+        }
+
+        /// <inheritdoc/>
+        protected override String GetStringRepresentation(Object value)
+        {
+            var vector = (Vector4)value;
+            return String.Format("({0}, {1}, {2}, {3})", vector.X, vector.Y, vector.Z, vector.W);
+        }
+    }
+}
