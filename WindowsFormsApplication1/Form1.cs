0fbbb7ce83ecbde95e59f6bd7de7528d3dd2d1ac
tlgkccampbell <webmaster@twistedlogik.net>
2014-11-05 15:09:39 -0500

    Added test application for design assemblies

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TwistedLogik.Ultraviolet;
+
+namespace WindowsFormsApplication1
+{
+    public class TestObject
+    {
+        public TestObject()
+        {
+            SomeMatrix = Matrix.Identity;
+        }
+
+        public Color SomeColor { get; set; }
+        public Vector2 SomeVector2 { get; set; }
+        public Vector3 SomeVector3 { get; set; }
+        public Vector4 SomeVector4 { get; set; }
+        public Matrix SomeMatrix { get; set; }
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            propertyGrid1.SelectedObject = new TestObject();
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            TwistedLogik.Nucleus.Design.TypeDescriptionMetadataLoader.Load(@"..\..\..\TwistedLogik.Ultraviolet.Design\Vector2.xml");
+            TwistedLogik.Nucleus.Design.TypeDescriptionMetadataLoader.Load(@"..\..\..\TwistedLogik.Ultraviolet.Design\Vector3.xml");
+            TwistedLogik.Nucleus.Design.TypeDescriptionMetadataLoader.Load(@"..\..\..\TwistedLogik.Ultraviolet.Design\Vector4.xml");
+            TwistedLogik.Nucleus.Design.TypeDescriptionMetadataLoader.Load(@"..\..\..\TwistedLogik.Ultraviolet.Design\Color.xml");
+            TwistedLogik.Nucleus.Design.TypeDescriptionMetadataLoader.Load(@"..\..\..\TwistedLogik.Ultraviolet.Design\Matrix.xml");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
