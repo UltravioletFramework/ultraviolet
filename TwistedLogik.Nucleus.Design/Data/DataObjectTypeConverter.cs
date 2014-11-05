64d33f17c8a6b52842f9e7d83cb4e4cd96492932
tlgkccampbell <webmaster@twistedlogik.net>
2014-11-05 13:54:37 -0500

    Added new type converters for basic data / content objects

---
+++ TwistedLogik.Nucleus.Design/Data/DataObjectTypeConverter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using TwistedLogik.Nucleus.Data;
+
+namespace TwistedLogik.Nucleus.Design.Data
+{
+    /// <summary>
+    ///  
+    /// </summary>
+    [CLSCompliant(false)]
+    public abstract class DataObjectTypeConverter<T, U> : ObjectResolverTypeConverter<T> where T : DataObject
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataObjectTypeConverter{T, U}"/> class.
+        /// </summary>
+        /// <param name="allowNone">A value indicating whether (none) is a valid selection.</param>
+        protected DataObjectTypeConverter(Boolean allowNone)
+        {
+            values = GetStandardValuesForRegistry(allowNone);
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this type converter supports standard values.
+        /// </summary>
+        public override Boolean GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether only standard values are allowed.
+        /// </summary>
+        public override Boolean GetStandardValuesExclusive(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Gets the type converter's collection of standard values.
+        /// </summary>
+        public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return values;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="allowNone"></param>
+        /// <returns></returns>
+        private static StandardValuesCollection GetStandardValuesForRegistry(Boolean allowNone)
+        {
+            var cache = allowNone ? StandardValuesPlusNoneCache : StandardValuesCache;
+
+            StandardValuesCollection values;
+            if (!cache.TryGetValue(typeof(T), out values))
+            {
+                var registry = DataObjectRegistries.Get<T>();
+                var keys = registry.Select(x => String.Format("@{0}:{1}", registry.ReferenceResolutionName, x.Key));
+                var references = keys.Select(x => (U)ObjectResolver.FromString(x, typeof(U))).ToList();
+                var referencesPlusNone = references.ToList();
+                referencesPlusNone.Insert(0, default(U));
+
+                StandardValuesCache[typeof(T)] = new StandardValuesCollection(references);
+                StandardValuesPlusNoneCache[typeof(T)] = new StandardValuesCollection(referencesPlusNone);
+
+                values = allowNone ? StandardValuesPlusNoneCache[typeof(T)] : StandardValuesCache[typeof(T)];
+            }
+
+            return values;
+        }
+
+        // The cache of standard values associated with each asset manifest.
+        private static readonly Dictionary<Type, StandardValuesCollection> StandardValuesPlusNoneCache =
+            new Dictionary<Type, StandardValuesCollection>();
+        private static readonly Dictionary<Type, StandardValuesCollection> StandardValuesCache =
+            new Dictionary<Type, StandardValuesCollection>();
+
+        // The standard values for this converter.
+        private StandardValuesCollection values;
+    }
+}
---
+++ TwistedLogik.Ultraviolet.Design/Content/ContentManifestTypeConverter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using TwistedLogik.Nucleus;
+using TwistedLogik.Nucleus.Data;
+using TwistedLogik.Ultraviolet.Content;
+
+namespace TwistedLogik.Ultraviolet.Design.Content
+{
+    /// <summary>
+    /// Represents a type converter which produces a list of asset identifier values corresponding
+    /// to the contents of a particular content manifest group.
+    /// </summary>
+    public abstract class ContentManifestTypeConverter : ObjectResolverTypeConverter<AssetID>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContentManifestTypeConverter"/> class.
+        /// </summary>
+        /// <param name="manifestGroup">The <see cref="ContentManifestGroup"/> from which to retrieve asset identifier values.</param>
+        public ContentManifestTypeConverter(ContentManifestGroup manifestGroup)
+        {
+            Contract.Require(manifestGroup, "manifestGroup");
+
+            values = GetStandardValuesForManifestGroup(manifestGroup);
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this type converter supports standard values.
+        /// </summary>
+        public override Boolean GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether only standard values are allowed.
+        /// </summary>
+        public override Boolean GetStandardValuesExclusive(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Gets the type converter's collection of standard values.
+        /// </summary>
+        public override TypeConverter.StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return values;
+        }
+
+        /// <summary>
+        /// Gets a <see cref="StandardValuesCollection"/> containing the asset identifiers defined by the specified content manifest group.
+        /// </summary>
+        /// <param name="manifestGroup">The <see cref="ContentManifestGroup"/> from which to retrieve asset identifier values.</param>
+        /// <returns>The <see cref="StandardValuesCollection"/> which was created.</returns>
+        private static StandardValuesCollection GetStandardValuesForManifestGroup(ContentManifestGroup manifestGroup)
+        {
+            StandardValuesCollection values;
+            if (!StandardValuesCache.TryGetValue(manifestGroup, out values))
+            {
+                var ids = from asset in manifestGroup select asset.CreateAssetID();
+
+                values = new StandardValuesCollection(ids.ToList());
+                StandardValuesCache[manifestGroup] = values;
+            }
+            return values;
+        }
+
+        // The cache of standard values associated with each manifest group.
+        private static readonly Dictionary<ContentManifestGroup, StandardValuesCollection> StandardValuesCache =
+            new Dictionary<ContentManifestGroup, StandardValuesCollection>();
+
+        // The standard values for this converter.
+        private StandardValuesCollection values;
+    }
+}
