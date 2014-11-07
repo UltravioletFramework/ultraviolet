37eacecae6dab98b964cc531f16684a2da583901
tlgkccampbell <webmaster@twistedlogik.net>
2014-11-07 14:58:44 -0500

    Added XML font definition generation

--- UvFont/Program.cs
+++ UvFont/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Drawing.Text;
+using System.Xml.Linq;
 
 namespace UvFont
 {
@@ -12,12 +14,13 @@ namespace UvFont
         public static void Main(string[] args)
         {
             var chars    = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
-            var fontname = "Segoe UI";
-            var fontsize = 24f;
+            var fontname = "Garamond";
+            var fontsize = 32f;
 
-            using (var font = new Font(fontname, fontsize * SupersampleFactor))
+            using (var font             = new Font(fontname, fontsize))
+            using (var fontSupersampled = new Font(fontname, fontsize * SupersampleFactor))
             {
-                var glyphs      = CreateGlyphImages(font, chars);
+                var glyphs      = CreateGlyphImages(fontSupersampled, chars);
                 var textureSize = 0;
                 var textureFits = CalculateTextureSize(glyphs, out textureSize);
                 if (!textureFits)
@@ -26,10 +29,8 @@ namespace UvFont
                     return;
                 }
 
-                using (var imgOutput = CreateOutputTexture(glyphs, textureSize))
-                {
-                    imgOutput.Save("output.png", ImageFormat.Png);
-                }
+                GenerateXmlFontDefinition(font, chars);
+                GenerateOutputTexture(font, glyphs, textureSize);
             }
         }
 
@@ -74,6 +75,21 @@ namespace UvFont
             return glyphs;
         }
 
+        private static String GetFontSafeName(Font font)
+        {
+            return font.FontFamily.Name.Replace(" ", String.Empty);
+        }
+
+        private static String GetFontDefinitionSafeName(Font font)
+        {
+            return String.Format("{0}.xml", GetFontSafeName(font));
+        }
+
+        private static String GetFontTextureSafeName(Font font)
+        {
+            return String.Format("{0}Texture.png", GetFontSafeName(font));
+        }
+
         private static Boolean CalculateTextureSize(IEnumerable<Bitmap> glyphs, out Int32 size)
         {
             size = 64;
@@ -110,34 +126,80 @@ namespace UvFont
             return true;
         }
 
-        private static Bitmap CreateOutputTexture(IEnumerable<Bitmap> glyphs, Int32 textureSize)
+        private static void GenerateOutputTexture(Font font, IEnumerable<Bitmap> glyphs, Int32 textureSize)
         {
-            var img = new Bitmap(textureSize, textureSize);
+            using (var img = new Bitmap(textureSize, textureSize))
+            {
+                using (var gfx = Graphics.FromImage(img))
+                {
+                    gfx.Clear(Color.Magenta);
 
+                    var x = 1;
+                    var y = 1;
+
+                    foreach (var glyph in glyphs)
+                    {
+                        if (x + glyph.Width >= textureSize)
+                        {
+                            x = 1;
+                            y = y + glyph.Height + 1;
+                        }
+
+                        gfx.SetClip(new Rectangle(x, y, glyph.Width, glyph.Height));
+                        gfx.Clear(Color.Transparent);
+                        gfx.DrawImageUnscaled(glyph, new Point(x, y));
+
+                        x = x + glyph.Width + 1;
+                    }
+                }
+
+                img.Save(GetFontTextureSafeName(font), ImageFormat.Png);
+            }
+        }
+
+        private static void GenerateXmlFontDefinition(Font font, String chars)
+        {
+            using (var img = new Bitmap(1, 1))
             using (var gfx = Graphics.FromImage(img))
             {
-                gfx.Clear(Color.Magenta);
-
-                var x = 1;
-                var y = 1;
-
-                foreach (var glyph in glyphs)
-                {
-                    if (x + glyph.Width >= textureSize)
+                var kernings = 
+                    from c1 in chars
+                    from c2 in chars
+                    let kerning = MeasureKerning(gfx, font, c1, c2)
+                    where
+                     c1 != c2
+                    select new
                     {
-                        x = 1;
-                        y = y + glyph.Height + 1;
-                    }
+                        KerningValue = kerning,
+                        KerningXml   = new XElement("Kerning", new XAttribute("Pair", String.Format("{0}{1}", c1, c2)), kerning)
+                    };
 
-                    gfx.SetClip(new Rectangle(x, y, glyph.Width, glyph.Height));
-                    gfx.Clear(Color.Transparent);
-                    gfx.DrawImageUnscaled(glyph, new Point(x, y));
+                var defaultAdjustment = (from k in kernings
+                                         group k by k.KerningValue into g
+                                         orderby g.Count() descending
+                                         select g).First().Key;
 
-                    x = x + glyph.Width + 1;
-                }
+                kernings = kernings.Where(x => x.KerningValue != defaultAdjustment);
+
+                var xml = new XDocument(new XDeclaration("1.0", "utf-8", "yes"),
+                    new XElement("SpriteFont",
+                        new XElement("Face", new XAttribute("Style", "Regular"),
+                            new XElement("Texture", GetFontTextureSafeName(font)),
+                            new XElement("Kernings", new XAttribute("DefaultAdjustment", defaultAdjustment),
+                                kernings.Select(x => x.KerningXml))
+                        )
+                    )
+                );
+                xml.Save(GetFontDefinitionSafeName(font));
             }
+        }
 
-            return img;
+        private static Int32 MeasureKerning(Graphics gfx, Font font, Char c1, Char c2)
+        {
+            var c1Size = gfx.MeasureString(c1.ToString(), font);
+            var c2Size = gfx.MeasureString(c2.ToString(), font);
+            var kernedSize = gfx.MeasureString(String.Format("{0}{1}", c1, c2), font);
+            return (Int32)(kernedSize.Width - (c1Size.Width + c2Size.Width));
         }
 
         private const Single SupersampleFactor = 4.0f;
