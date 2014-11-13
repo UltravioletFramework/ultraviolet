d99b34fdd78058d390024f6b0dd02f620b4f5b0b
Cole Campbell <webmaster@twistedlogik.net>
2014-11-13 17:30:17 -0500

    Implemented ContentArchive

---
+++ TwistedLogik.Ultraviolet/Content/ContentArchive.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TwistedLogik.Nucleus;
+
+namespace TwistedLogik.Ultraviolet.Content
+{
+    /// <summary>
+    /// Represents an archive containing multiple content files.
+    /// </summary>
+    public sealed class ContentArchive : IDisposable, IEnumerable<ContentArchiveNode>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContentArchive"/> class.
+        /// </summary>
+        /// <param name="roots">A collection containing the archive's root nodes.</param>
+        private ContentArchive(IEnumerable<ContentArchiveNode> roots)
+        {
+            this.isReadOnly = false;
+            this.stream     = null;
+            this.reader     = null;
+            this.roots      = new List<ContentArchiveNode>(roots);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContentArchive"/> class.
+        /// </summary>
+        /// <param name="stream">The stream from which to load the content archive.</param>
+        private ContentArchive(Stream stream)
+        {
+            this.isReadOnly = true;
+            this.stream     = stream;
+            this.reader     = new BinaryReader(stream);
+
+            var fileHeader = reader.ReadString();
+            if (fileHeader != "UVARC0")
+                throw new InvalidDataException("TODO");
+
+            var rootCount = reader.ReadInt32();
+            for (int i = 0; i < rootCount; i++)
+            {
+                var root = ContentArchiveNode.FromArchive(reader);
+                roots.Add(root);
+            }
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="ContentArchive"/> from the specified set of directories.
+        /// </summary>
+        /// <param name="directories">A collection of directories from which to create the content archive.</param>
+        /// <returns>The <see cref="ContentArchive"/> that was created.</returns>
+        public static ContentArchive FromFileSystem(IEnumerable<String> directories)
+        {
+            Contract.Require(directories, "directories");
+
+            return new ContentArchive(directories.Select(x => ContentArchiveNode.FromFileSystem(x)));
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="ContentArchive"/> from the specified stream.
+        /// </summary>
+        /// <param name="stream">The <see cref="Stream"/> that contains the archive data.</param>
+        /// <returns>The <see cref="ContentArchive"/> that was created.</returns>
+        public static ContentArchive FromArchiveFile(Stream stream)
+        {
+            Contract.Require(stream, "stream");
+
+            return new ContentArchive(stream);
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Writes the content archive to the specified stream.
+        /// </summary>
+        /// <param name="writer">A <see cref="BinaryWriter"/> on the stream to which to save the archive.</param>
+        public void Save(BinaryWriter writer)
+        {
+            Contract.Require(writer, "writer");
+            Contract.EnsureNot(isReadOnly, "TODO");
+
+            writer.Write("UVARC0");
+            writer.Write(roots.Count());
+
+            var position = 0L;
+
+            foreach (var root in roots)
+            {
+                root.WriteIndex(writer, ref position);
+            }
+
+            foreach (var root in roots)
+            {
+                root.WriteData(writer);
+            }
+        }
+
+        /// <summary>
+        /// Finds the archive node with the specified path.
+        /// </summary>
+        /// <param name="path">The relative path of the asset to load.</param>
+        /// <param name="throwIfNotFound">A value indicating whether to throw a <see cref="FileNotFoundException"/> if the path does not exist.</param>
+        /// <returns>The archive node with the specified path, or null if no such node exists and <paramref name="throwIfNotFound"/> is <c>false</c>.</returns>
+        public ContentArchiveNode Find(String path, Boolean throwIfNotFound = true)
+        {
+            Contract.Require(path, "path");
+
+            ContentArchiveNode cachedNode;
+            if (pathCache.TryGetValue(path, out cachedNode))
+            {
+                return cachedNode;
+            }
+
+            var components = path.Split(DirectorySeparators);
+            var rootname   = components.First();
+            var root       = roots.SingleOrDefault(x => String.Equals(x.Name, rootname, StringComparison.InvariantCultureIgnoreCase));
+
+            if (root == null)
+            {
+                if (throwIfNotFound)
+                {
+                    throw new DirectoryNotFoundException(path);
+                }
+                return null;
+            }
+
+            var current = root;
+            for (var i = 1; i < components.Length; i++)
+            {
+                var component = components[i];
+
+                if (component == ".")
+                    continue;
+
+                if (component == "..")
+                {
+                    current = current.Parent;
+                    if (current == null)
+                    {
+                        if (throwIfNotFound)
+                        {
+                            throw new FileNotFoundException(path);
+                        }
+                    }
+                    continue;
+                }
+
+                var match = current.Children.SingleOrDefault(x => String.Equals(x.Name, component, StringComparison.InvariantCultureIgnoreCase));
+                if (match == null)
+                {
+                    if (throwIfNotFound)
+                    {
+                        throw new FileNotFoundException(path);
+                    }
+                    return null;
+                }
+                current = match;
+            }
+
+            pathCache[path] = current;
+            return current;
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the <see cref="ContentArchive"/>.
+        /// </summary>
+        /// <returns>An enumerator that iterates through the <see cref="ContentArchive"/>.</returns>
+        List<ContentArchiveNode>.Enumerator GetEnumerator()
+        {
+            return roots.GetEnumerator();
+        }
+
+        /// <inheritdoc/>
+        IEnumerator<ContentArchiveNode> IEnumerable<ContentArchiveNode>.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <inheritdoc/>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Releases resources associated with the object.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> if the object is being disposed; <c>false</c> if the object is being finalized.</param>
+        private void Dispose(Boolean disposing)
+        {
+            if (disposing)
+            {
+                SafeDispose.Dispose(reader);
+                SafeDispose.Dispose(stream);
+            }
+        }
+
+        // Property values.
+        private Boolean isReadOnly;
+
+        // The archive's data stream.
+        private readonly Stream stream;
+        private readonly BinaryReader reader;
+
+        // The archive's root nodes.
+        private readonly List<ContentArchiveNode> roots = 
+            new List<ContentArchiveNode>();
+
+        // The archive's path cache.
+        private readonly Dictionary<String, ContentArchiveNode> pathCache = 
+            new Dictionary<String, ContentArchiveNode>();
+
+        // The set of characters used to delimit directories in a path.
+        private static readonly Char[] DirectorySeparators = new[] { 
+            Path.DirectorySeparatorChar, 
+            Path.AltDirectorySeparatorChar };
+    }
+}
---
+++ TwistedLogik.Ultraviolet/Content/ContentArchiveNode.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TwistedLogik.Nucleus;
+
+namespace TwistedLogik.Ultraviolet.Content
+{
+    /// <summary>
+    /// Represents a file or directory in an Ultraviolet content archive.
+    /// </summary>
+    public sealed class ContentArchiveNode
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContentArchiveNode"/> class from a file or directory in the file system.
+        /// </summary>
+        /// <param name="parent">The node's parent node.</param>
+        /// <param name="path">The path to the file or directory that this node represents.</param>
+        private ContentArchiveNode(ContentArchiveNode parent, String path)
+        {
+            path = System.IO.Path.GetFullPath(path);
+
+            this.parent      = parent;
+            this.path        = path;
+            this.name        = System.IO.Path.GetFileName(path);
+            this.isFile      = File.Exists(path);
+            this.isDirectory = Directory.Exists(path);
+
+            if (!isFile && !isDirectory)
+                throw new FileNotFoundException(path);
+
+            if (isFile)
+            {
+                using (var stream = File.OpenRead(path))
+                {
+                    this.sizeInBytes = stream.Length;
+                }
+            }
+            else
+            {
+                LoadChildren(path);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContentArchiveNode"/> class from an archive stream.
+        /// </summary>
+        /// <param name="parent">The node's parent node.</param>
+        /// <param name="reader">A <see cref="BinaryReader"/> on the stream containing the archive data.</param>
+        private ContentArchiveNode(ContentArchiveNode parent, BinaryReader reader)
+        {
+            this.parent      = parent;
+            this.name        = reader.ReadString();
+            this.isFile      = reader.ReadBoolean();
+            this.isDirectory = !this.isFile;
+
+            if (this.isFile)
+            {
+                this.position    = reader.ReadInt64();
+                this.sizeInBytes = reader.ReadInt64();
+            }
+            else
+            {
+                var childList = new List<ContentArchiveNode>();
+                var childCount = reader.ReadInt32();
+
+                for (int i = 0; i < childCount; i++)
+                {
+                    var node = new ContentArchiveNode(this, reader);
+                    childList.Add(node);
+                }
+
+                this.children = childList;
+            }
+        }
+
+        /// <summary>
+        /// Creates an archive tree that represents the specified path in the file system.
+        /// </summary>
+        /// <param name="path">The path for which to build an archive tree.</param>
+        /// <returns>An <see cref="ContentArchiveNode"/> that represents the root of the constructed archive tree.</returns>
+        internal static ContentArchiveNode FromFileSystem(String path)
+        {
+            Contract.RequireNotEmpty(path, "path");
+
+            return new ContentArchiveNode(null, path);
+        }
+
+        /// <summary>
+        /// Creates an archive tree that represents the data contained in the specified archive stream.
+        /// </summary>
+        /// <param name="reader">A <see cref="BinaryReader"/> on the stream that contains the archive data.</param>
+        /// <returns>An <see cref="ContentArchiveNode"/> that represents the root of the constructed archive tree.</returns>
+        internal static ContentArchiveNode FromArchive(BinaryReader reader)
+        {
+            Contract.Require(reader, "reader");
+
+            return new ContentArchiveNode(null, reader);
+        }
+
+        /// <inheritdoc/>
+        public override String ToString()
+        {
+            return name;
+        }
+
+        /// <summary>
+        /// Writes the node's index data, and the index data of its descendants, to the specified stream.
+        /// </summary>
+        /// <param name="writer">A <see cref="BinaryWriter"/> on the stream to which to write the node data.</param>
+        /// <param name="position">The position of the first node's data within the stream.</param>
+        public void WriteIndex(BinaryWriter writer, ref Int64 position)
+        {
+            Contract.Require(writer, "writer");
+
+            WriteIndexInternal(writer, ref position);
+        }
+
+        /// <summary>
+        /// Serializes the node and its descendants to the specified stream.
+        /// </summary>
+        /// <param name="writer">A <see cref="BinaryWriter"/> on the stream to which to serialize the node.</param>
+        public void WriteData(BinaryWriter writer)
+        {
+            Contract.Require(writer, "writer");
+
+            if (path == null)
+                throw new InvalidOperationException("TODO");
+
+            if (IsFile)
+            {
+                var buffer = new Byte[1024 * 10];
+
+                using (var stream = File.OpenRead(path))
+                {
+                    while (true)
+                    {
+                        var read = stream.Read(buffer, 0, buffer.Length);
+                        if (read > 0)
+                            writer.Write(buffer, 0, read);
+
+                        if (read < buffer.Length)
+                            break;
+                    }
+                }
+            }
+
+            foreach (var child in Children)
+            {
+                child.WriteData(writer);
+            }
+        }
+
+        /// <summary>
+        /// Gets the node's parent node.
+        /// </summary>
+        public ContentArchiveNode Parent
+        {
+            get { return parent; }
+        }
+
+        /// <summary>
+        /// Gets the node's name.
+        /// </summary>
+        public String Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the node represents a file.
+        /// </summary>
+        public Boolean IsFile
+        {
+            get
+            {
+                return isFile;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the node represents a directory.
+        /// </summary>
+        public Boolean IsDirectory
+        {
+            get
+            {
+                return isDirectory;
+            }
+        }
+
+        /// <summary>
+        /// Gets the node's size in bytes.
+        /// </summary>
+        public Int64 SizeInBytes
+        {
+            get
+            {
+                return sizeInBytes;
+            }
+        }
+
+        /// <summary>
+        /// Gets the archive node's child nodes.
+        /// </summary>
+        public IEnumerable<ContentArchiveNode> Children
+        {
+            get
+            {
+                return children ?? Enumerable.Empty<ContentArchiveNode>();
+            }
+        }
+
+        /// <summary>
+        /// Loads the node's child nodes from the file system.
+        /// </summary>
+        /// <param name="path">The full path that the node represents.</param>
+        private void LoadChildren(String path)
+        {
+            var list = new List<ContentArchiveNode>();
+
+            foreach (var directory in Directory.GetDirectories(path))
+            {
+                var node = new ContentArchiveNode(this, directory);
+                list.Add(node);
+            }
+
+            foreach (var file in Directory.GetFiles(path))
+            {
+                var node = new ContentArchiveNode(this, file);
+                list.Add(node);
+            }
+            
+            this.children = list;
+        }
+
+        /// <summary>
+        /// Writes the 
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="position"></param>
+        private void WriteIndexInternal(BinaryWriter writer, ref Int64 position)
+        {
+            writer.Write(this.Name);
+            writer.Write(this.IsFile);
+
+            if (this.IsFile)
+            {
+                writer.Write(0L);
+                writer.Write(this.SizeInBytes);
+            }
+            else
+            {
+                writer.Write(this.Children.Count());
+                foreach (var child in children)
+                {
+                    child.WriteIndexInternal(writer, ref position);
+                }
+            }
+        }
+
+        // Property values.
+        private readonly ContentArchiveNode parent;
+        private readonly String path;
+        private readonly String name;
+        private readonly Boolean isFile;
+        private readonly Boolean isDirectory;
+        private readonly Int64 position;
+        private readonly Int64 sizeInBytes;
+        private IEnumerable<ContentArchiveNode> children;
+    }
+}
