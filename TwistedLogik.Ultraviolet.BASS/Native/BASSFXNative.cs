a4443d9b0dcdad616b4bd7bea7f44ad98a9cff7f
tlgkccampbell <webmaster@twistedlogik.net>
2014-11-24 13:41:11 -0500

    Changed BASS_FX p/invoke functions to stdcall

--- TwistedLogik.Ultraviolet.BASS/Native/BASSFXNative.cs
+++ TwistedLogik.Ultraviolet.BASS/Native/BASSFXNative.cs
@@ -13,10 +13,10 @@ namespace TwistedLogik.Ultraviolet.BASS.Native
             LibraryLoader.Load("bass_fx");
         }
 
-        [DllImport("bass_fx", EntryPoint = "BASS_FX_GetVersion", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("bass_fx", EntryPoint = "BASS_FX_GetVersion", CallingConvention = CallingConvention.StdCall)]
         public static extern UInt32 GetVersion();
 
-        [DllImport("bass_fx", EntryPoint = "BASS_FX_TempoCreate", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("bass_fx", EntryPoint = "BASS_FX_TempoCreate", CallingConvention = CallingConvention.StdCall)]
         public static extern UInt32 TempoCreate(UInt32 chan, UInt32 flags);
     }
 }
