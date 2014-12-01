e93d723c12c5d8fde20373f7193b8ce87b8d4252
tlgkccampbell <webmaster@twistedlogik.net>
2014-12-01 16:03:14 -0500

    Added attachment properties to Canvas

--- TwistedLogik.Ultraviolet.Layout/Elements/Canvas.cs
+++ TwistedLogik.Ultraviolet.Layout/Elements/Canvas.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TwistedLogik.Nucleus;
 
 namespace TwistedLogik.Ultraviolet.Layout.Elements
 {
@@ -18,6 +19,122 @@ namespace TwistedLogik.Ultraviolet.Layout.Elements
         {
 
         }
+
+        /// <summary>
+        /// Gets the distance between the left edge of the canvas and the left edge of the specified element.
+        /// </summary>
+        /// <param name="element">The element to evaluate.</param>
+        /// <returns>The distance between the left edge of the canvas and the left edge of the specified element.</returns>
+        public static Int32 GetLeft(UIElement element)
+        {
+            Contract.Require(element, "element");
+
+            return element.GetValue<Int32>(LeftProperty);
+        }
+
+        /// <summary>
+        /// Gets the distance between the top edge of the canvas and the top edge of the specified element.
+        /// </summary>
+        /// <param name="element">The element to evaluate.</param>
+        /// <returns>The distance between the top edge of the canvas and the top edge of the specified element.</returns>
+        public static Int32 GetTop(UIElement element)
+        {
+            Contract.Require(element, "element");
+
+            return element.GetValue<Int32>(TopProperty);
+        }
+
+        /// <summary>
+        /// Gets the distance between the right edge of the canvas and the right edge of the specified element.
+        /// </summary>
+        /// <param name="element">The element to evaluate.</param>
+        /// <returns>The distance between the right edge of the canvas and the right edge of the specified element.</returns>
+        public static Int32 GetRight(UIElement element)
+        {
+            Contract.Require(element, "element");
+
+            return element.GetValue<Int32>(RightProperty);
+        }
+
+        /// <summary>
+        /// Gets the distance between the bottom edge of the canvas and the bottom edge of the specified element.
+        /// </summary>
+        /// <param name="element">The element to evaluate.</param>
+        /// <returns>The distance between the bottom edge of the canvas and the bottom edge of the specified element.</returns>
+        public static Int32 GetBottom(UIElement element)
+        {
+            Contract.Require(element, "element");
+
+            return element.GetValue<Int32>(BottomProperty);
+        }
+
+        /// <summary>
+        /// Sets the distance between the left edge of the canvas and the left edge of the specified element.
+        /// </summary>
+        /// <param name="element">The element to modify.</param>
+        /// <param name="value">The distance between the left edge of the canvas and the left edge of the specified element.</param>
+        public static void SetLeft(UIElement element, Int32 value)
+        {
+            Contract.Require(element, "element");
+
+            element.SetValue<Int32>(LeftProperty, value);
+        }
+
+        /// <summary>
+        /// Sets the distance between the top edge of the canvas and the top edge of the specified element.
+        /// </summary>
+        /// <param name="element">The element to modify.</param>
+        /// <param name="value">The distance between the top edge of the canvas and the top edge of the specified element.</param>
+        public static void SetTop(UIElement element, Int32 value)
+        {
+            Contract.Require(element, "element");
+
+            element.SetValue<Int32>(TopProperty, value);
+        }
+
+        /// <summary>
+        /// Sets the distance between the right edge of the canvas and the right edge of the specified element.
+        /// </summary>
+        /// <param name="element">The element to modify.</param>
+        /// <param name="value">The distance between the right edge of the canvas and the right edge of the specified element.</param>
+        public static void SetRight(UIElement element, Int32 value)
+        {
+            Contract.Require(element, "element");
+
+            element.SetValue<Int32>(RightProperty, value);
+        }
+
+        /// <summary>
+        /// Sets the distance between the bottom edge of the canvas and the bottom edge of the specified element.
+        /// </summary>
+        /// <param name="element">The element to modify.</param>
+        /// <param name="value">The distance between the bottom edge of the canvas and the bottom edge of the specified element.</param>
+        public static void SetBottom(UIElement element, Int32 value)
+        {
+            Contract.Require(element, "element");
+
+            element.SetValue<Int32>(BottomProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating the distance between the left edge of the canvas and the left edge of the element.
+        /// </summary>
+        public static readonly DependencyProperty LeftProperty = DependencyProperty.Register("Left", typeof(Int32), typeof(Canvas));
+
+        /// <summary>
+        /// Gets or sets a value indicating the distance between the top edge of the canvas and the top edge of the element.
+        /// </summary>
+        public static readonly DependencyProperty TopProperty = DependencyProperty.Register("Top", typeof(Int32), typeof(Canvas));
+
+        /// <summary>
+        /// Gets or sets a value indicating the distance between the right edge of the canvas and the right edge of the element.
+        /// </summary>
+        public static readonly DependencyProperty RightProperty = DependencyProperty.Register("Right", typeof(Int32), typeof(Canvas));
+
+        /// <summary>
+        /// Gets or sets a value indicating the distance between the bottom edge of the canvas and the bottom edge of the element.
+        /// </summary>
+        public static readonly DependencyProperty BottomProperty = DependencyProperty.Register("Bottom", typeof(Int32), typeof(Canvas));
 
         /// <inheritdoc/>
         protected override Rectangle CalculateLayoutArea(UIElement child)
