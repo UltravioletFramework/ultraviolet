1948f9befd8cf5b64fbfa641eaff9188448c00f6
Cole Campbell <webmaster@twistedlogik.net>
2014-12-14 16:06:34 -0500

    Added missing animation files

---
+++ TwistedLogik.Ultraviolet.Layout/Animation/CharAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TwistedLogik.Ultraviolet.Layout.Animation
+{
+    /// <summary>
+    /// Represents an animation between char values.
+    /// </summary>
+    public sealed class CharAnimation : Animation<Char>
+    {
+        /// <inheritdoc/>
+        public override Char InterpolateValues(Char value1, Char value2, EasingFunction easing, Single factor)
+        {
+            return Tweening.Tween(value1, value2, easing, factor);
+        }
+    }
+}
---
+++ TwistedLogik.Ultraviolet.Layout/Animation/Int16Animation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TwistedLogik.Ultraviolet.Layout.Animation
+{
+    /// <summary>
+    /// Represents an animation between 16-bit integers.
+    /// </summary>
+    public sealed class Int16Animation : Animation<Int16>
+    {
+        /// <inheritdoc/>
+        public override Int16 InterpolateValues(Int16 value1, Int16 value2, EasingFunction easing, Single factor)
+        {
+            return Tweening.Tween(value1, value2, easing, factor);
+        }
+    }
+}
