f4944cadb5dd140198dc7e8676faa9cdcbc81662
Cole Campbell <webmaster@twistedlogik.net>
2015-01-19 22:11:18 -0500

    Fixed a memory leak caused by enumerating the list of classes in a UVSS selector.

---
+++ TwistedLogik.Ultraviolet/UI/Presentation/Styles/UvssClassCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwistedLogik.Ultraviolet.UI.Presentation.Styles
+{
+    /// <summary>
+    /// Represents a collection of UVSS classes.
+    /// </summary>
+    public sealed partial class UvssClassCollection : IEnumerable<String>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UvssClassCollection"/> class.
+        /// </summary>
+        /// <param name="classes">A collection of class names with which to initialize the collection.</param>
+        internal UvssClassCollection(IEnumerable<String> classes)
+        {
+            this.storage = (classes == null) ? new List<String>() : new List<String>(classes);
+        }
+
+        /// <summary>
+        /// Gets the number of items in the collection.
+        /// </summary>
+        public Int32 Count
+        {
+            get { return storage.Count; }
+        }
+
+        // State values.
+        private readonly List<String> storage;
+    }
+}
