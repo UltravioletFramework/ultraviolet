7d13ad3190b897d1755c4b2bd92b007a60b98f56
Cole Campbell <webmaster@twistedlogik.net>
2015-01-31 13:11:32 -0500

    Grid's virtual cells are now assigned a measurement priority.

--- TwistedLogik.Ultraviolet.UI.Presentation/Elements/Grid.VirtualCellMetadata.cs
+++ TwistedLogik.Ultraviolet.UI.Presentation/Elements/Grid.VirtualCellMetadata.cs
@@ -11,6 +11,14 @@ namespace TwistedLogik.Ultraviolet.UI.Presentation.Elements
         private class VirtualCellMetadata
         {
             /// <summary>
+            /// Updates the value of the <see cref="MeasurementPriority"/> property.
+            /// </summary>
+            public void UpdateMeasurementPriority()
+            {
+                MeasurementPriority = CalculateMeasurementPriority();
+            }
+
+            /// <summary>
             /// Gets or sets the child element that the cell represents.
             /// </summary>
             public UIElement Element
@@ -89,6 +97,54 @@ namespace TwistedLogik.Ultraviolet.UI.Presentation.Elements
             {
                 get;
                 set;
+            }
+
+            /// <summary>
+            /// Gets a value which indicates the order in which this cell is measured
+            /// compared to other virtual cells in the grid.
+            /// </summary>
+            public Int32 MeasurementPriority
+            {
+                get;
+                private set;
+            }
+
+            /// <summary>
+            /// Calculates the measurement priority for this cell.
+            /// </summary>
+            /// <returns>The cell's measurement priority based on its current state.</returns>
+            private Int32 CalculateMeasurementPriority()
+            {
+                /*
+                 * PRIORITY 0: These can always be measured because they depend only on
+                 *             a statically-specified size or the size of the cell's content.
+                 * PRIORITY 1: Width depends on statically-specified size or cell content.
+                 *             Height depends on a star row.
+                 * PRIORITY 2: Width depends on a star column.
+                 *             Height depends on statically-specified size or cell content.
+                 * PRIORITY 3: Both width and height depend on star rows/columns.
+                 * 
+                 * Note that we deviate from this a bit as a performance optimization; cells
+                 * which are star/px sized are assigned priority 3 to avoid measuring them twice.
+                 */
+
+                if (ContainsStarRows && ContainsStarColumns)
+                {
+                    // NOTE: See note about perf optimization above
+                    if (ContainsAutoColumns)
+                    {
+                        return 1;
+                    }
+                    return 3;
+                }
+
+                if (ContainsStarColumns && !ContainsStarRows)
+                    return 2;
+
+                if (ContainsStarRows && !ContainsStarColumns)
+                    return 1;
+
+                return 0;
             }
         }
     }
