760d25e798b0f9e565fe01f416a856fde7332ee7
Cole Campbell <webmaster@twistedlogik.net>
2015-02-14 16:55:43 -0500

    Began work on ItemsControl.

---
+++ TwistedLogik.Ultraviolet.UI.Presentation/Elements/ItemCollection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using TwistedLogik.Nucleus;
+using TwistedLogik.Nucleus.Collections;
+
+namespace TwistedLogik.Ultraviolet.UI.Presentation.Elements
+{
+    /// <summary>
+    /// Represents the collection which makes up the content of an <see cref="ItemsControl"/>.
+    /// </summary>
+    public sealed class ItemCollection : ObservableList<Object>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ItemCollection"/> class.
+        /// </summary>
+        /// <param name="owner">The <see cref="ItemsControl"/> that owns the collection.</param>
+        public ItemCollection(ItemsControl owner)
+        {
+            Contract.Require(owner, "owner");
+
+            this.owner = owner;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="ItemsControl"/> that owns the collection.
+        /// </summary>
+        internal ItemsControl Owner
+        {
+            get { return owner; }
+        }
+
+        /// <inheritdoc/>
+        protected override void OnItemAdded(object item)
+        {
+            base.OnItemAdded(item);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnItemRemoved(object item)
+        {
+            base.OnItemRemoved(item);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnCleared()
+        {
+            base.OnCleared();
+        }
+
+        // Property values.
+        private readonly ItemsControl owner;
+    }
+}
---
+++ TwistedLogik.Ultraviolet.UI.Presentation/Elements/ItemsControl.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+
+namespace TwistedLogik.Ultraviolet.UI.Presentation.Elements
+{
+    /// <summary>
+    /// Represents a control which presents the user with a list of items to select.
+    /// </summary>
+    public class ItemsControl : Control
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ItemsControl"/> class.
+        /// </summary>
+        /// <param name="uv">The Ultraviolet context.</param>
+        /// <param name="id">The unique identifier of this element within its layout.</param>
+        public ItemsControl(UltravioletContext uv, String id)
+            : base(uv, id)
+        {
+            this.items = new ItemCollection(this);
+        }
+
+        /// <summary>
+        /// Gets the collection that contains the control's items.
+        /// </summary>
+        public ItemCollection Items
+        {
+            get { return items; }
+        }
+
+        /// <summary>
+        /// Gets or sets the collection which is used to generate the control's items.
+        /// </summary>
+        public IEnumerable ItemSource
+        {
+            get { return GetValue<IEnumerable>(ItemSourceProperty); }
+            set { SetValue<IEnumerable>(ItemSourceProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the formatting string which is used to format the control's items,
+        /// if they are displayed as strings.
+        /// </summary>
+        public String ItemStringFormat
+        {
+            get { return GetValue<String>(ItemStringFormatProperty); }
+            set { SetValue<String>(ItemStringFormatProperty, value); }
+        }
+
+        /// <summary>
+        /// Occurs when the value of the <see cref="ItemSource"/> property changes.
+        /// </summary>
+        public event UIElementEventHandler ItemSourceChanged;
+
+        /// <summary>
+        /// Occurs when the value of the <see cref="ItemStringFormat"/> property changes.
+        /// </summary>
+        public event UIElementEventHandler ItemStringFormatChanged;
+
+        /// <summary>
+        /// Identifies the <see cref="ItemSource"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ItemSourceProperty = DependencyProperty.Register("ItemSource", typeof(IEnumerable), typeof(ItemsControl),
+            new DependencyPropertyMetadata(HandleItemSourceChanged, null, DependencyPropertyOptions.None));
+
+        /// <summary>
+        /// Identifies the <see cref="ItemStringFormat"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ItemStringFormatProperty = DependencyProperty.Register("ItemStringFormat", typeof(String), typeof(ItemsControl),
+            new DependencyPropertyMetadata(HandleItemStringFormatChanged, null, DependencyPropertyOptions.None));
+
+        /// <summary>
+        /// Raises the <see cref="ItemSourceChanged"/> event.
+        /// </summary>
+        protected virtual void OnItemSourceChanged()
+        {
+            var temp = ItemSourceChanged;
+            if (temp != null)
+            {
+                temp(this);
+            }
+        }
+
+        /// <summary>
+        /// Raises the <see cref="ItemStringFormatChanged"/> event.
+        /// </summary>
+        protected virtual void OnItemStringFormatChanged()
+        {
+            var temp = ItemStringFormatChanged;
+            if (temp != null)
+            {
+                temp(this);
+            }
+        }
+
+        /// <summary>
+        /// Occurs when the value of the <see cref="ItemSource"/> dependency property changes.
+        /// </summary>
+        /// <param name="dobj">The dependency object that raised the event.</param>
+        private static void HandleItemSourceChanged(DependencyObject dobj)
+        {
+            var itemControl = (ItemsControl)dobj;
+            itemControl.OnItemSourceChanged();
+        }
+
+        /// <summary>
+        /// Occurs when the value of the <see cref="ItemStringFormat"/> dependency property changes.
+        /// </summary>
+        /// <param name="dobj">The dependency object that raised the event.</param>
+        private static void HandleItemStringFormatChanged(DependencyObject dobj)
+        {
+            var itemControl = (ItemsControl)dobj;
+            itemControl.OnItemStringFormatChanged();
+        }
+
+        // Property values,
+        private readonly ItemCollection items;
+    }
+}
