77b0d8d10e016edf0329a1c545efe352b4351e43
tlgkccampbell <webmaster@twistedlogik.net>
2015-02-17 15:23:43 -0500

    Changed INotifyPropertyChanged to specify property name instead of object instance.

--- TwistedLogik.Nucleus/Data/INotifyPropertyChanged.cs
+++ TwistedLogik.Nucleus/Data/INotifyPropertyChanged.cs
@@ -5,8 +5,9 @@ namespace TwistedLogik.Nucleus.Data
     /// <summary>
     /// Represents the method that is called when an instance of the <see cref="INotifyPropertyChanged"/> interface is changed.
     /// </summary>
-    /// <param name="instance">The instance that was changed.</param>
-    public delegate void NotifyChangedEventHandler(Object instance);
+    /// <param name="propertyName">The name of the property that was changed. If all of the object's properties have
+    /// changed, this value can be either <see cref="String.Empty"/> or <c>null</c>.</param>
+    public delegate void NotifyChangedEventHandler(String propertyName);
 
     /// <summary>
     /// Represents an object which raises an event when one of its property values changes.
