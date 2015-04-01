b84364a5451e6819f7483c8ced3fcb40ba2f72b1
tlgkccampbell <cole.campbell@twistedlogik.net>
2015-03-31 23:27:13 -0400

    Checked in some missing files.

---
+++ UvTestViewer/Controllers/RenderingTestController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using UvTestViewer.Models;
+using UvTestViewer.Services;
+
+namespace UvTestViewer.Controllers
+{
+    public class RenderingTestController : Controller
+    {
+        // GET: RenderingTest
+        public ActionResult Index()
+        {
+            var service  = new RenderingTestService();
+            var overview = service.GetMostRecentRenderingTestOverview();
+
+            return View(overview ?? new RenderingTestOverview());
+        }
+    }
+}
---
+++ UvTestViewer/Models/RenderingTestOverview.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UvTestViewer.Models
+{
+    /// <summary>
+    /// The view model for the rendering test page.
+    /// </summary>
+    public class RenderingTestOverview
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RenderingTestOverview"/> class.
+        /// </summary>
+        public RenderingTestOverview()
+        {
+            Tests = Enumerable.Empty<RenderingTest>();
+        }
+
+        /// <summary>
+        /// Gets or sets the identifier of the test run.
+        /// </summary>
+        public Int64 TestRunID
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the time at which tests were processed.
+        /// </summary>
+        public DateTime TimeProcessed
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the set of tests to display.
+        /// </summary>
+        public IEnumerable<RenderingTest> Tests
+        {
+            get;
+            set;
+        }
+    }
+}
