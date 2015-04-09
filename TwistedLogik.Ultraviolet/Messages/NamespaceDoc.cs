4e27cecfde2fafc05ac377f7ffe9f9e340e3d34e
tlgkccampbell <cole.campbell@twistedlogik.net>
2015-04-09 14:49:29 -0400

    Added missing NamespaceDoc.

---
+++ TwistedLogik.Ultraviolet/Messages/NamespaceDoc.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TwistedLogik.Ultraviolet.Messages
+{
+    /// <summary>
+    /// The <see cref="TwistedLogik.Ultraviolet.Messages"/> namespace contains custom message types used by
+    /// the Ultraviolet Framework to faciliate communication between its subsystems.
+    /// </summary>
+    [CompilerGenerated]
+    class NamespaceDoc
+    {
+
+    }
+}
