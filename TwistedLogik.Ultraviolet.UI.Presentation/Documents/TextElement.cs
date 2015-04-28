ec4eb54c167d86edfd64090fb5fa70d772e7dacc
tlgkccampbell <cole.campbell@twistedlogik.net>
2015-04-28 09:00:42 -0400

    TextElement now properly declares its dependency properties as attached properties.

--- TwistedLogik.Ultraviolet.UI.Presentation/Documents/TextElement.cs
+++ TwistedLogik.Ultraviolet.UI.Presentation/Documents/TextElement.cs
@@ -10,25 +10,25 @@ namespace TwistedLogik.Ultraviolet.UI.Presentation.Documents
         /// <summary>
         /// Identifies the Font dependency property.
         /// </summary>
-        public static readonly DependencyProperty FontProperty = DependencyProperty.Register("Font", typeof(SourcedResource<SpriteFont>), typeof(TextElement),
+        public static readonly DependencyProperty FontProperty = DependencyProperty.RegisterAttached("Font", typeof(SourcedResource<SpriteFont>), typeof(TextElement),
             new PropertyMetadata<SourcedResource<SpriteFont>>(null, PropertyMetadataOptions.AffectsMeasure));
 
         /// <summary>
         /// Identifies the FontStyle dependency property.
         /// </summary>
-        public static readonly DependencyProperty FontStyleProperty = DependencyProperty.Register("FontStyle", typeof(SpriteFontStyle), typeof(TextElement),
+        public static readonly DependencyProperty FontStyleProperty = DependencyProperty.RegisterAttached("FontStyle", typeof(SpriteFontStyle), typeof(TextElement),
            new PropertyMetadata<SpriteFontStyle>(UltravioletBoxedValues.SpriteFontStyle.Regular, PropertyMetadataOptions.AffectsMeasure));
 
         /// <summary>
         /// Identifies the Background dependency property.
         /// </summary>
-        public static readonly DependencyProperty BackgroundProperty = DependencyProperty.Register("Background", typeof(Color), typeof(TextElement),
+        public static readonly DependencyProperty BackgroundProperty = DependencyProperty.RegisterAttached("Background", typeof(Color), typeof(TextElement),
             new PropertyMetadata<Color>(UltravioletBoxedValues.Color.White));
 
         /// <summary>
         /// Identifies the Foreground dependency property.
         /// </summary>
-        public static readonly DependencyProperty ForegroundProperty = DependencyProperty.Register("Foreground", typeof(Color), typeof(TextElement),
+        public static readonly DependencyProperty ForegroundProperty = DependencyProperty.RegisterAttached("Foreground", typeof(Color), typeof(TextElement),
             new PropertyMetadata<Color>(UltravioletBoxedValues.Color.Black));
     }
 }
