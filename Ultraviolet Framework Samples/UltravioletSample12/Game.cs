2bbf9beff7d701c2e9240f29c434a6af92b51319
tlgkccampbell <cole.campbell@twistedlogik.net>
2015-05-26 15:05:26 -0400

    Cleaned up sample projects.

---
+++ Ultraviolet Framework Samples/UltravioletSample12/Game.cs
@@ -0,0 +1,35 @@
+﻿using TwistedLogik.Ultraviolet;
+using TwistedLogik.Ultraviolet.OpenGL;
+
+namespace UltravioletSample
+{
+#if ANDROID
+    [Android.App.Activity(Label = "Ultraviolet Sample 12", MainLauncher = true, ConfigurationChanges = 
+        Android.Content.PM.ConfigChanges.Orientation | 
+        Android.Content.PM.ConfigChanges.ScreenSize | 
+        Android.Content.PM.ConfigChanges.KeyboardHidden)]
+    public class Game : UltravioletActivity
+#else
+    public class Game : UltravioletApplication
+#endif
+    {
+        public Game()
+            : base("TwistedLogik", "Ultraviolet Sample 12")
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+
+        protected override UltravioletContext OnCreatingUltravioletContext()
+        {
+            return new OpenGLUltravioletContext(this);
+        }
+    }
+}
