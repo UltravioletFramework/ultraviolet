4baddeec0874f19b0e9b4939abc085d18f3b0ea4
tlgkccampbell <cole.campbell@twistedlogik.net>
2015-05-30 20:42:05 -0400

    Incremented Nucleus' version to 4.2.0.0.

--- TwistedLogik.Nucleus/Properties/AssemblyInfo.cs
+++ TwistedLogik.Nucleus/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Security;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.1.0.0")]
-[assembly: AssemblyFileVersion("4.1.2.0")]
+[assembly: AssemblyVersion("4.2.0.0")]
+[assembly: AssemblyFileVersion("4.2.0.0")]
