8e2cb479eedb084fa610e6accca87967241a4b12
tlgkccampbell <cole.campbell@twistedlogik.net>
2015-05-31 13:46:56 -0400

    Fixed some issues with assembly signing configuration.

--- UltravioletGame (Android)/Resources/Resource.Designer.cs
+++ UltravioletGame (Android)/Resources/Resource.Designer.cs
@@ -92,14 +92,14 @@ namespace UltravioletGame
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040002
-			public const int ApplicationName = 2130968578;
-			
 			// aapt resource value: 0x7f040001
-			public const int Hello = 2130968577;
+			public const int ApplicationName = 2130968577;
 			
 			// aapt resource value: 0x7f040000
-			public const int app_name = 2130968576;
+			public const int Hello = 2130968576;
+			
+			// aapt resource value: 0x7f040002
+			public const int app_name = 2130968578;
 			
 			static String()
 			{
