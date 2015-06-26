9e415a5eb77296a8c0a9928ad079ce0189c4c96f
tlgkccampbell <cole.campbell@twistedlogik.net>
2015-06-26 15:18:25 -0400

    Remove and sort usings.

--- TwistedLogik.Ultraviolet Tests/UI/Presentation/Screens/VerticalWrapPanelTestScreenViewModel.cs
+++ TwistedLogik.Ultraviolet Tests/UI/Presentation/Screens/VerticalWrapPanelTestScreenViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace TwistedLogik.Ultraviolet.Tests.UI.Presentation.Screens
 {
     public class VerticalWrapPanelTestScreenViewModel { }
