fe12b964823f60ec832aab008d613eaaa191f613
tlgkccampbell <cole.campbell@twistedlogik.net>
2015-07-10 23:18:53 -0400

    Improved icon loading on OSX.

--- TwistedLogik.Ultraviolet.OSX/Platform/OSXIconLoader.cs
+++ TwistedLogik.Ultraviolet.OSX/Platform/OSXIconLoader.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 using TwistedLogik.Ultraviolet.Desktop.Graphics;
 using TwistedLogik.Ultraviolet.Graphics;
 using TwistedLogik.Ultraviolet.Platform;
 using MonoMac.AppKit;
+using MonoMac.Foundation;
 
 namespace TwistedLogik.Ultraviolet.OSX
 {
@@ -12,31 +14,47 @@ namespace TwistedLogik.Ultraviolet.OSX
 		/// <inheritdoc/>
 		public override Surface2D LoadIcon()
 		{
-			var assembly = Assembly.GetEntryAssembly();
-			var assemblyLocation = (assembly == null) ? typeof(UltravioletContext).Assembly.Location : assembly.Location;
-
-			/* HACK: Trying to load an icon from a network path throws an exception, which is a problem
-             * given the way the test servers are currently configured. So just skip loading it. */
-			var uri = new Uri(assemblyLocation);
-			if (uri.IsUnc)
+			var bundle = NSBundle.MainBundle;
+			if (bundle == null)
 				return null;
 
-			var icon = NSWorkspace.SharedWorkspace.IconForFile(assemblyLocation);
-			if (icon == null)
-			{
-				throw new InvalidOperationException();
-			}
-
+			var icon = default(NSImage);
 			try
 			{
+				try
+				{
+					if (String.Equals(Path.GetExtension(bundle.BundlePath), ".app", StringComparison.OrdinalIgnoreCase))
+					{
+						icon = NSWorkspace.SharedWorkspace.IconForFile(bundle.BundlePath);
+						if (icon == null)
+						{
+							throw new InvalidOperationException();
+						}
+					} 
+					else
+					{
+						using (var stream = typeof(UltravioletContext).Assembly.GetManifestResourceStream("TwistedLogik.Ultraviolet.uv.ico"))
+						{
+							icon = NSImage.FromStream(stream);
+						}
+					}
+				} 
+				catch (FileNotFoundException)
+				{
+					return null;
+				}
+
 				using (var source = new OSXSurfaceSource(icon))
 				{
 					return Surface2D.Create(source);
 				}
-			}
+			} 
 			finally
 			{
-				icon.Dispose();
+				if (icon != null)
+				{
+					icon.Dispose();
+				}
 			}
 		}
 	}
