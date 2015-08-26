aa04dd749e4ef57e7c036852be78444fbb6962b7
Cole Campbell <cole.campbell@twistedlogik.net>
2015-08-26 18:06:35 -0400

    Implemented AdornerDecorator

---
+++ TwistedLogik.Ultraviolet.UI.Presentation/Documents/Adorner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TwistedLogik.Ultraviolet.UI.Presentation.Documents
+{
+    /// <summary>
+    /// Represents an element which decorates another selement.
+    /// </summary>
+    [UvmlKnownType]
+    public class Adorner : FrameworkElement
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Adorner"/> class.
+        /// </summary>
+        /// <param name="uv">The Ultraviolet context.</param>
+        /// <param name="name">The element's identifying name within its namescope.</param>
+        public Adorner(UltravioletContext uv, String name)
+            : base(uv, name)
+        {
+
+        }
+    }
+}
---
+++ TwistedLogik.Ultraviolet.UI.Presentation/Documents/AdornerLayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TwistedLogik.Ultraviolet.UI.Presentation.Documents
+{
+    /// <summary>
+    /// Represents a layer for containing adorners
+    /// </summary>
+    [UvmlKnownType]
+    public class AdornerLayer : FrameworkElement
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdornerLayer"/> class.
+        /// </summary>
+        /// <param name="uv">The Ultraviolet context.</param>
+        /// <param name="name">The element's identifying name within its namescope.</param>
+        public AdornerLayer(UltravioletContext uv, String name)
+            : base(uv, name)
+        {
+
+        }
+    }
+}
