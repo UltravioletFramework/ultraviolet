77a4f6d47b2745e27e8a3a19a9f156d42b64dac0
Cole Campbell <cole.campbell@twistedlogik.net>
2015-09-06 19:45:28 -0400

    Updated Linux script

--- Ultraviolet Framework Samples/Dependencies/update.sh
+++ Ultraviolet Framework Samples/Dependencies/update.sh
@@ -10,9 +10,7 @@ cp "../../Binaries/AnyCPU/Release/TwistedLogik.Ultraviolet.xml" TwistedLogik.Ult
 cp "../../Binaries/AnyCPU/Release/TwistedLogik.Ultraviolet.OpenGL.dll" TwistedLogik.Ultraviolet.OpenGL.dll
 cp "../../Binaries/AnyCPU/Release/TwistedLogik.Ultraviolet.OpenGL.xml" TwistedLogik.Ultraviolet.OpenGL.xml 2>/dev/null
 cp "../../Binaries/AnyCPU/Release/TwistedLogik.Ultraviolet.SDL2.dll" TwistedLogik.Ultraviolet.SDL2.dll
-cp "../../Binaries/AnyCPU/Release/TwistedLogik.Ultraviolet.SDL2.dll.config" TwistedLogik.Ultraviolet.SDL2.dll.config
 cp "../../Binaries/AnyCPU/Release/TwistedLogik.Ultraviolet.BASS.dll" TwistedLogik.Ultraviolet.BASS.dll
-cp "../../Binaries/AnyCPU/Release/TwistedLogik.Ultraviolet.BASS.dll.config" TwistedLogik.Ultraviolet.BASS.dll.config
 cp "../../Binaries/AnyCPU/Release/TwistedLogik.Ultraviolet.Desktop.dll" TwistedLogik.Ultraviolet.Desktop.dll
 cp "../../Binaries/AnyCPU/Release/TwistedLogik.Ultraviolet.UI.Presentation.dll" TwistedLogik.Ultraviolet.UI.Presentation.dll
 
