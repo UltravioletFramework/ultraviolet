47886eac922cdc00f8e1cb3786963f80b505f504
Cole Campbell <cole.campbell@twistedlogik.net>
2015-09-06 10:44:50 -0400

    Misc build fixes

--- TwistedLogik.Gluon (Android)/Properties/AssemblyInfo.cs
+++ TwistedLogik.Gluon (Android)/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-
 [assembly: CLSCompliant(false)]
+
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
