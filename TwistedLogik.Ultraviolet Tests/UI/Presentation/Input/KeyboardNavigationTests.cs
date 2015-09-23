8f3dda070148a5e4697c7eaa1bb32c793c68c294
tlgkccampbell <cole.campbell@twistedlogik.net>
2015-09-23 14:29:18 -0400

    Added more keynav tests

--- TwistedLogik.Ultraviolet Tests/UI/Presentation/Input/KeyboardNavigationTests.cs
+++ TwistedLogik.Ultraviolet Tests/UI/Presentation/Input/KeyboardNavigationTests.cs
@@ -116,6 +116,48 @@ namespace TwistedLogik.Ultraviolet.Tests.UI.Presentation.Input
 
         [TestMethod]
         [TestCategory("UPF")]
+        public void UPF_KeyNav_TabNavigatesCorrectly_WithTabIndices()
+        {
+            GivenAPresentationFoundationTestFor(content => new UPF_KeyNav_TabIndices(content))
+                .OnFrame(1, app => TheElementWithFocus(app).ShouldBeNull())
+                .OnFrame(2, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, false))
+                .OnFrame(3, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb2"))
+                .OnFrame(4, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, false))
+                .OnFrame(5, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb5"))
+                .OnFrame(6, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, false))
+                .OnFrame(7, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb4"))
+                .OnFrame(8, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, false))
+                .OnFrame(9, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb1"))
+                .OnFrame(10, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, false))
+                .OnFrame(11, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb3"))
+                .OnFrame(12, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, false))
+                .OnFrame(13, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb2"))
+                .RunAllFrameActions();
+        }
+
+        [TestMethod]
+        [TestCategory("UPF")]
+        public void UPF_KeyNav_TabNavigatesCorrectly_WithTabIndices_AndKeyboardNavigationModeIsLocal()
+        {
+            GivenAPresentationFoundationTestFor(content => new UPF_KeyNav_TabIndicesLocal(content))
+                .OnFrame(1, app => TheElementWithFocus(app).ShouldBeNull())
+                .OnFrame(2, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, false))
+                .OnFrame(3, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb5"))
+                .OnFrame(4, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, false))
+                .OnFrame(5, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb1"))
+                .OnFrame(6, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, false))
+                .OnFrame(7, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb2"))
+                .OnFrame(8, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, false))
+                .OnFrame(9, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb4"))
+                .OnFrame(10, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, false))
+                .OnFrame(11, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb3"))
+                .OnFrame(12, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, false))
+                .OnFrame(13, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb5"))
+                .RunAllFrameActions();
+        }
+
+        [TestMethod]
+        [TestCategory("UPF")]
         public void UPF_KeyNav_ShiftTabMovesToPreviousElement()
         {
             GivenAPresentationFoundationTestFor(content => new UPF_KeyNav_Simple(content))
@@ -213,6 +255,48 @@ namespace TwistedLogik.Ultraviolet.Tests.UI.Presentation.Input
                 .OnFrame(9, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb2"))
                 .OnFrame(10, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, true))
                 .OnFrame(11, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb2"))
+                .RunAllFrameActions();
+        }
+
+        [TestMethod]
+        [TestCategory("UPF")]
+        public void UPF_KeyNav_ShiftTabNavigatesCorrectly_WithTabIndices()
+        {
+            GivenAPresentationFoundationTestFor(content => new UPF_KeyNav_TabIndices(content))
+                .OnFrame(1, app => TheElementWithFocus(app).ShouldBeNull())
+                .OnFrame(2, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, true))
+                .OnFrame(3, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb3"))
+                .OnFrame(4, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, true))
+                .OnFrame(5, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb1"))
+                .OnFrame(6, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, true))
+                .OnFrame(7, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb4"))
+                .OnFrame(8, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, true))
+                .OnFrame(9, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb5"))
+                .OnFrame(10, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, true))
+                .OnFrame(11, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb2"))
+                .OnFrame(12, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, true))
+                .OnFrame(13, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb3"))
+                .RunAllFrameActions();
+        }
+
+        [TestMethod]
+        [TestCategory("UPF")]
+        public void UPF_KeyNav_ShiftTabNavigatesCorrectly_WithTabIndices_AndKeyboardNavigationModeIsLocal()
+        {
+            GivenAPresentationFoundationTestFor(content => new UPF_KeyNav_TabIndicesLocal(content))
+                .OnFrame(1, app => TheElementWithFocus(app).ShouldBeNull())
+                .OnFrame(2, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, true))
+                .OnFrame(3, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb3"))
+                .OnFrame(4, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, true))
+                .OnFrame(5, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb4"))
+                .OnFrame(6, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, true))
+                .OnFrame(7, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb2"))
+                .OnFrame(8, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, true))
+                .OnFrame(9, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb1"))
+                .OnFrame(10, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, true))
+                .OnFrame(11, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb5"))
+                .OnFrame(12, app => app.SpoofKeyPress(Scancode.Tab, Key.Tab, false, false, true))
+                .OnFrame(13, app => TheElementWithFocus<TextBox>(app).ShouldSatisfyTheCondition(x => x.Name == "tb3"))
                 .RunAllFrameActions();
         }
     }
---
+++ TwistedLogik.Ultraviolet Tests/UI/Presentation/Screens/UPF_KeyNav_TabIndicesLocal.cs
@@ -0,0 +1,13 @@
+﻿using TwistedLogik.Ultraviolet.Content;
+
+namespace TwistedLogik.Ultraviolet.Tests.UI.Presentation.Screens
+{
+    public class UPF_KeyNav_TabIndicesLocal : TestScreenBase<UPF_KeyNav_TabIndicesLocal_VM>
+    {
+        public UPF_KeyNav_TabIndicesLocal(ContentManager globalContent)
+            : base("Content/UI/Screens/UPF_KeyNav_TabIndicesLocal", "View", globalContent)
+        {
+
+        }
+    }
+}
