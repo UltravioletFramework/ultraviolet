022cfa458b1ead7c7f3121bce1aaa38db9827799
Cole Campbell <cole.campbell@twistedlogik.net>
2015-09-28 23:32:14 -0400

    Added focus indicators to CheckBox and RadioButton

--- TwistedLogik.Ultraviolet.UI.Presentation/Controls/RadioButton.cs
+++ TwistedLogik.Ultraviolet.UI.Presentation/Controls/RadioButton.cs
@@ -10,6 +10,14 @@ namespace TwistedLogik.Ultraviolet.UI.Presentation.Controls
     public class RadioButton : ToggleButton
     {
         /// <summary>
+        /// Initializes the <see cref="RadioButton"/> type.
+        /// </summary>
+        static RadioButton()
+        {
+            VerticalContentAlignmentProperty.OverrideMetadata(typeof(RadioButton), new PropertyMetadata<VerticalAlignment>(VerticalAlignment.Top));
+        }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="RadioButton"/> class.
         /// </summary>
         /// <param name="uv">The Ultraviolet context.</param>
