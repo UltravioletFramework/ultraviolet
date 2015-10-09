a02eda31b582696dbdb10cfab4650fdc8f809d73
tlgkccampbell <cole.campbell@twistedlogik.net>
2015-10-09 13:43:18 -0400

    Added IsDefault, IsCancel, and IsDefaulted dependency properties to Button

--- TwistedLogik.Ultraviolet.UI.Presentation/Controls/Button.cs
+++ TwistedLogik.Ultraviolet.UI.Presentation/Controls/Button.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TwistedLogik.Nucleus;
 using TwistedLogik.Ultraviolet.UI.Presentation.Controls.Primitives;
 
 namespace TwistedLogik.Ultraviolet.UI.Presentation.Controls
@@ -27,6 +28,79 @@ namespace TwistedLogik.Ultraviolet.UI.Presentation.Controls
             : base(uv, name)
         {
 
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this is the default button for the current view.
+        /// </summary>
+        public Boolean IsDefault
+        {
+            get { return GetValue<Boolean>(IsDefaultProperty); }
+            set { SetValue(IsDefaultProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this is the cancel button for the current view.
+        /// </summary>
+        public Boolean IsCancel
+        {
+            get { return GetValue<Boolean>(IsCancelProperty); }
+            set { SetValue(IsCancelProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the button is activated when the user presses the Enter key.
+        /// </summary>
+        public Boolean IsDefaulted
+        {
+            get { return GetValue<Boolean>(IsDefaultedProperty); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="IsDefault"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty IsDefaultProperty = DependencyProperty.Register("IsDefault", typeof(Boolean), typeof(Button),
+            new PropertyMetadata<Boolean>(CommonBoxedValues.Boolean.False, PropertyMetadataOptions.None, HandleIsDefaultChanged));
+
+        /// <summary>
+        /// Identifies the <see cref="IsCancel"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty IsCancelProperty = DependencyProperty.Register("IsCancel", typeof(Boolean), typeof(Button),
+            new PropertyMetadata<Boolean>(CommonBoxedValues.Boolean.False, PropertyMetadataOptions.None, HandleIsCancelChanged));
+
+        /// <summary>
+        /// The private access key for the <see cref="IsDefault"/> read-only dependency property.
+        /// </summary>
+        private static readonly DependencyPropertyKey IsDefaultedPropertyKey = DependencyProperty.RegisterReadOnly("IsDefaulted", typeof(Boolean), typeof(Button),
+            new PropertyMetadata<Boolean>(CommonBoxedValues.Boolean.False, PropertyMetadataOptions.None, HandleIsDefaultedChanged));
+
+        /// <summary>
+        /// Identifies the <see cref="IsDefaulted"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty IsDefaultedProperty = IsDefaultedPropertyKey.DependencyProperty;
+
+        /// <summary>
+        /// Occurs when the value of the <see cref="IsDefault"/> dependency property changes.
+        /// </summary>
+        private static void HandleIsDefaultChanged(DependencyObject dobj, Boolean oldValue, Boolean newValue)
+        {
+            // TODO
+        }
+
+        /// <summary>
+        /// Occurs when the value of the <see cref="IsCancel"/> dependency property changes.
+        /// </summary>
+        private static void HandleIsCancelChanged(DependencyObject dobj, Boolean oldValue, Boolean newValue)
+        {
+            // TODO
+        }
+
+        /// <summary>
+        /// Occurs when the value of the <see cref="IsDefaulted"/> dependency property changes.
+        /// </summary>
+        private static void HandleIsDefaultedChanged(DependencyObject dobj, Boolean oldValue, Boolean newValue)
+        {
+            // TODO
         }
     }
 }
