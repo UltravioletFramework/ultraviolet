a470d011feb87c8ffbd11f15abbd4b1543af3d50
Cole Campbell <cole.campbell@twistedlogik.net>
2015-11-19 23:21:55 -0500

    Updated signasm.bat

--- Binaries/AnyCPU/Production/signasm.bat
+++ Binaries/AnyCPU/Production/signasm.bat
@@ -11,6 +11,9 @@ ECHO %TLSN%
 ECHO.
 ECHO Strong naming assemblies...
 
+IF "%1"=="test_nucleus" GOTO test_nucleus
+IF "%1"=="test_ultraviolet" GOTO test_ultraviolet
+
 sn.exe -R "TwistedLogik.Nucleus.dll" "%TLSN%" 
 sn.exe -R "TwistedLogik.Nucleus.Design.dll" "%TLSN%"
 sn.exe -R "TwistedLogik.Gluon.dll" "%TLSN%" 
@@ -25,3 +28,18 @@ sn.exe -R "TwistedLogik.Ultraviolet.Desktop.dll" "%TLSN%"
 sn.exe -R "TwistedLogik.Ultraviolet.Tooling.dll" "%TLSN%"
 sn.exe -R "uvfont.exe" "%TLSN%"
 sn.exe -R "uvarchive.exe" "%TLSN%"
+
+:test_nucleus
+sn.exe -R "TwistedLogik.Nucleus.dll" "%TLSN%" 
+GOTO :eof
+
+:test_ultraviolet
+sn.exe -R "TwistedLogik.Nucleus.dll" "%TLSN%" 
+sn.exe -R "TwistedLogik.Gluon.dll" "%TLSN%" 
+sn.exe -R "TwistedLogik.Ultraviolet.dll" "%TLSN%"
+sn.exe -R "TwistedLogik.Ultraviolet.UI.Presentation.dll" "%TLSN%"
+sn.exe -R "TwistedLogik.Ultraviolet.OpenGL.dll" "%TLSN%"
+sn.exe -R "TwistedLogik.Ultraviolet.SDL2.dll" "%TLSN%"
+sn.exe -R "TwistedLogik.Ultraviolet.BASS.dll" "%TLSN%"
+sn.exe -R "TwistedLogik.Ultraviolet.Desktop.dll" "%TLSN%"
+GOTO :eof
