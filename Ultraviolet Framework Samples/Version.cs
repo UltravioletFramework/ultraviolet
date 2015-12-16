608db9dc50836c1d80afe23a00df6689b510425c
Cole Campbell <cole.campbell@twistedlogik.net>
2015-12-16 16:24:11 -0500

    Added a text transform file to generate version info from build metadata.

--- Ultraviolet Framework Samples/Version.cs
+++ Ultraviolet Framework Samples/Version.cs
@@ -2,7 +2,8 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("Ultraviolet Framework")]
 [assembly: AssemblyCompany("TwistedLogik Software")]
-[assembly: AssemblyCopyright("Copyright © Cole Campbell 2014-2016")]
+[assembly: AssemblyCopyright("Copyright © Cole Campbell 2014-2015")]
 
 [assembly: AssemblyVersion("1.3.0.0")]
 [assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0.0 dev build")]
