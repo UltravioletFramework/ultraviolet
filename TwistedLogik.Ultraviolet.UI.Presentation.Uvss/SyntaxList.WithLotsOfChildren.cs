992e6f00c8d608956ab24ee9166ec0042d09cfc3
Cole Campbell <cole.campbell@twistedlogik.net>
2015-12-29 15:31:56 -0500

    Check in missing file

---
+++ TwistedLogik.Ultraviolet.UI.Presentation.Uvss/SyntaxList.WithLotsOfChildren.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TwistedLogik.Ultraviolet.UI.Presentation.Uvss
+{
+    partial class SyntaxList
+    {
+        /// <summary>
+        /// Represents a syntax list with lots of children.
+        /// </summary>
+        internal sealed class WithLotsOfChildren : WithManyChildrenBase
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="WithLotsOfChildren"/> class.
+            /// </summary>
+            /// <param name="children">An array containing the list's children.</param>
+            internal WithLotsOfChildren(ArrayElement<SyntaxNode>[] children)
+                : base(children)
+            {
+
+            }
+        }
+    }
+}
