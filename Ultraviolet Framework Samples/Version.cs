d5b83e82b2d7cc0d37630e82bf82ed6399678753
Cole Campbell <cole.campbell@twistedlogik.net>
2016-01-14 01:23:48 -0500

    Added Uvss assembly to samples

--- Ultraviolet Framework Samples/Version.cs
+++ Ultraviolet Framework Samples/Version.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("Ultraviolet Framework")]
 [assembly: AssemblyCompany("TwistedLogik Software")]
-[assembly: AssemblyCopyright("Copyright © Cole Campbell 2014-2015")]
+[assembly: AssemblyCopyright("Copyright © Cole Campbell 2014-2016")]
 
 [assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
-[assembly: AssemblyInformationalVersion("1.3.1.0 dev build")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
+[assembly: AssemblyInformationalVersion("1.3.2.0 dev build")]
