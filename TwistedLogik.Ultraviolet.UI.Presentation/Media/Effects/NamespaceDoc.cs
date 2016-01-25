6e93b54cca7a27fa90f3a10f1fd2491f3583330f
Cole Campbell <cole.campbell@twistedlogik.net>
2016-01-25 17:11:22 -0500

    Added some missing namespace docs.

---
+++ TwistedLogik.Ultraviolet.UI.Presentation/Media/Effects/NamespaceDoc.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TwistedLogik.Ultraviolet.UI.Presentation.Media.Effects
+{
+	/// <summary>
+	/// The <see cref="TwistedLogik.Ultraviolet.UI.Presentation.Media.Effects"/> namespace contains types
+	/// which can be used to apply graphical effects to interface elements.
+	/// </summary>
+	[CompilerGenerated]
+    class NamespaceDoc
+    {
+
+    }
+}
