fb0037388ffbae97d08878c194b5c0d9c20a65ad
Cole Campbell <cole.campbell@twistedlogik.net>
2016-01-26 02:33:54 -0500

    Added more missing namespace docs.

---
+++ TwistedLogik.Ultraviolet.UI.Presentation.Uvss/Diagnostics/NamespaceDoc.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TwistedLogik.Ultraviolet.UI.Presentation.Uvss.Diagnostics
+{
+	/// <summary>
+	/// The <see cref="TwistedLogik.Ultraviolet.UI.Presentation.Uvss.Diagnostics"/> namespace contains types
+	/// which are used to provide diagnostic information for an Ultraviolet StyleSheets (UVSS) syntax tree.
+	/// </summary>
+	[CompilerGenerated]
+    class NamespaceDoc
+    {
+
+    }
+}
---
+++ TwistedLogik.Ultraviolet.UI.Presentation.Uvss/Syntax/NamespaceDoc.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TwistedLogik.Ultraviolet.UI.Presentation.Uvss.Syntax
+{
+	/// <summary>
+	/// The <see cref="TwistedLogik.Ultraviolet.UI.Presentation.Uvss.Syntax"/> namespace contains types
+	/// which represents nodes in an Ultraviolet StyleSheets (UVSS) abstract syntax tree.
+	/// </summary>
+	[CompilerGenerated]
+    class NamespaceDoc
+    {
+
+    }
+}
