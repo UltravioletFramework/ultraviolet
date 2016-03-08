ff9ebc692f06092be03f87c29a6d7c304e979f9f
Cole Campbell <cole.campbell@twistedlogik.net>
2016-03-08 16:11:53 -0500

    Added NamespaceDoc to TwistedLogik.Ultraviolet.UI.Presentation.Uvml

---
+++ TwistedLogik.Ultraviolet.UI.Presentation/Uvml/NamespaceDoc.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TwistedLogik.Ultraviolet.UI.Presentation.Uvml
+{
+    /// <summary>
+    /// The <see cref="TwistedLogik.Ultraviolet.UI.Presentation.Uvml"/> namespace contains types which
+    /// are used in the construction of UVML templates.
+    /// </summary>
+    [CompilerGenerated]
+    class NamespaceDoc
+    {
+
+    }
+}
