33ddca864cc38ded5a75e5498e444fe97a6851b2
Cole Campbell <cole.campbell@twistedlogik.net>
2016-03-09 01:29:21 -0500

    Added symbols and friendly names to NuSpec packages.

--- NuGet/pack.bat
+++ NuGet/pack.bat
@@ -1,13 +1,13 @@
-nuget pack TwistedLogik.Gluon.nuspec
-nuget pack TwistedLogik.Nucleus.nuspec
-nuget pack TwistedLogik.Ultraviolet.Android.nuspec
+nuget pack TwistedLogik.Gluon.nuspec -Symbols
+nuget pack TwistedLogik.Nucleus.nuspec -Symbols
+nuget pack TwistedLogik.Ultraviolet.Android.nuspec -Symbols
 nuget pack TwistedLogik.Ultraviolet.BASS.Native.nuspec
-nuget pack TwistedLogik.Ultraviolet.BASS.nuspec
-nuget pack TwistedLogik.Ultraviolet.Desktop.nuspec
-nuget pack TwistedLogik.Ultraviolet.nuspec
-nuget pack TwistedLogik.Ultraviolet.OpenGL.nuspec
+nuget pack TwistedLogik.Ultraviolet.BASS.nuspec -Symbols
+nuget pack TwistedLogik.Ultraviolet.Desktop.nuspec -Symbols
+nuget pack TwistedLogik.Ultraviolet.nuspec -Symbols
+nuget pack TwistedLogik.Ultraviolet.OpenGL.nuspec -Symbols
 nuget pack TwistedLogik.Ultraviolet.OSX.nuspec
 nuget pack TwistedLogik.Ultraviolet.SDL2.Native.nuspec
-nuget pack TwistedLogik.Ultraviolet.SDL2.nuspec
-nuget pack TwistedLogik.Ultraviolet.UI.Presentation.Compiler.nuspec
-nuget pack TwistedLogik.Ultraviolet.UI.Presentation.nuspec
+nuget pack TwistedLogik.Ultraviolet.SDL2.nuspec -Symbols
+nuget pack TwistedLogik.Ultraviolet.UI.Presentation.Compiler.nuspec -Symbols
+nuget pack TwistedLogik.Ultraviolet.UI.Presentation.nuspec -Symbols
