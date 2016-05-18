75a1d6d46f47494115687690717673dc69219259
Cole Campbell <cole.campbell@twistedlogik.net>
2016-05-18 09:49:29 -0400

    Fix typos in NuGet files

--- NuGet/TwistedLogik.Ultraviolet.Game.Android/content/Game.cs.pp
+++ NuGet/TwistedLogik.Ultraviolet.Game.Android/content/Game.cs.pp
@@ -15,13 +15,7 @@ namespace $RootNamespace$
         public Game() 
             : base("YOUR_ORGANIZATION", "PROJECT_NAME") 
         { 
-            // NOTE:
-            // To start the game, create a new instance and invoke the Run() method:
-            // 
-            // using (var game = new Game()) 
-            // {
-            //     game.Run();
-            // }
+
         }
 
         protected override UltravioletContext OnCreatingUltravioletContext()
