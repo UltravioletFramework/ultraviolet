79830169000368db872f18842f7f195a0e3d7be3
Cole Campbell <cole.campbell@twistedlogik.net>
2016-06-01 09:32:25 -0400

    Move SpriteFont test

--- TwistedLogik.Ultraviolet Tests/Graphics/Graphics2D/SpriteFontTests.cs
+++ TwistedLogik.Ultraviolet Tests/Graphics/Graphics2D/SpriteFontTests.cs
@@ -133,40 +133,5 @@ namespace TwistedLogik.Ultraviolet.Tests.Graphics.Graphics2D
             TheResultingImage(result)
               .ShouldMatch(@"Resources/Expected/Graphics/Graphics2D/SpriteFont_LoadsAndRendersCorrectly_FromPreprocessedAsset.png");
         }
-
-        [Test]
-        [Category("Rendering")]
-        [Description("Ensures that sprite fonts can be used to draw East Asian characters.")]
-        public void SpriteFont_CorrectlyRendersEastAsianCharacters()
-        {
-            var spriteBatch = default(SpriteBatch);
-            var spriteFont = default(SpriteFont);
-
-            var result = GivenAnUltravioletApplication()
-                .WithContent(content =>
-                {
-                    spriteBatch = SpriteBatch.Create();
-                    spriteFont = content.Load<SpriteFont>("Fonts/MSGothic16");
-                })
-                .Render(uv =>
-                {
-                    spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
-
-                    // From the Japanese Wikipedia article "日本"
-                    spriteBatch.DrawString(spriteFont.Regular, 
-                        "日本国（にっぽんこく、にほんこく）、または日本\n" +
-                        "（にっぽん、にほん）は、東アジアに位置する日本\n" +
-                        "列島（北海道・本州・四国・九州の主要四島および\n" +
-                        "それに付随する島々）及び、南西諸島・小笠原諸島\n" +
-                        "などの諸島嶼から成る島国である。日本語が事実上\n" +
-                        "の公用語として使用されている。首都は事実上東京\n" +
-                        "都とされている。", Vector2.Zero, Color.White);
-
-                    spriteBatch.End();
-                });
-            
-            TheResultingImage(result)
-                .ShouldMatch(@"Resources/Expected/Graphics/Graphics2D/SpriteFont_CorrectlyRendersEastAsianCharacters.png");
-        }
     }
 }
