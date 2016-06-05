9fc8a6ca25a5c8343fb5a8c69d5ed1340169ee7e
Cole Campbell <cole.campbell@twistedlogik.net>
2016-06-04 20:33:38 -0400

    Add JSON loading test for TextTableLayout

--- TwistedLogik.Ultraviolet Tests/Graphics/Graphics2D/Text/TextTableTests.cs
+++ TwistedLogik.Ultraviolet Tests/Graphics/Graphics2D/Text/TextTableTests.cs
@@ -83,5 +83,57 @@ namespace TwistedLogik.Ultraviolet.Tests.Graphics.Graphics2D.Text
             TheResultingImage(result).
                 ShouldMatch(@"Resources/Expected/Graphics/Graphics2D/Text/TextTable_RendersFromViewModel.png");
         }
+
+        [Test]
+        [Category("Rendering")]
+        [Description("Ensures that the TextTable class is correctly deserialized from JSON.")]
+        public void TextTable_LoadsAndRendersCorrectly_FromJson()
+        {
+            const Int32 TablePadding = 4;
+            const Int32 TableOffset = 32;
+
+            var spriteBatch = default(SpriteBatch);
+            var spriteFont = default(SpriteFont);
+            var textRenderer = default(TextRenderer);
+            var table = default(TextTable<TextTableViewModel>);
+            var tableLayout = default(TextTableLayout);
+            var tableTexture = default(Texture2D);
+
+            var result = GivenAnUltravioletApplication()
+                .WithContent(content =>
+                {
+                    spriteBatch = SpriteBatch.Create();
+                    spriteFont = content.Load<SpriteFont>("Fonts/SegoeUI");
+
+                    textRenderer = new TextRenderer();
+                    textRenderer.RegisterFont("header", content.Load<SpriteFont>("Fonts/Garamond"));
+                    textRenderer.RegisterFont("text", content.Load<SpriteFont>("Fonts/SegoeUI"));
+
+                    tableTexture = Texture2D.Create(1, 1);
+                    tableTexture.SetData(new[] { Color.White });
+
+                    tableLayout = content.Load<TextTableLayout>("Tables/TestTextTableJson");
+                    table = tableLayout.Create<TextTableViewModel>(textRenderer, spriteFont);
+                })
+                .Render(uv =>
+                {
+                    uv.GetGraphics().Clear(Color.Magenta);
+
+                    spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+
+                    table.PerformLayout();
+
+                    spriteBatch.Draw(tableTexture, new RectangleF(TableOffset, TableOffset,
+                        table.ActualWidth + (TablePadding * 2),
+                        table.ActualHeight + (TablePadding * 2)), Color.Black * 0.75f);
+
+                    table.Draw(spriteBatch, Vector2.One * (TableOffset + TablePadding));
+
+                    spriteBatch.End();
+                });
+            
+            TheResultingImage(result).
+                ShouldMatch(@"Resources/Expected/Graphics/Graphics2D/Text/TextTable_LoadsAndRendersCorrectly_FromJson.png");
+        }
     }
 }
