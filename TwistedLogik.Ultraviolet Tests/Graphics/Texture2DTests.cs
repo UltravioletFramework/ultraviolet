69ea19f38f3f3e6536d661728ad5a91906f6f56b
Cole Campbell <cole.campbell@twistedlogik.net>
2016-06-04 21:18:23 -0400

    Added test for preprocessed textures

--- TwistedLogik.Ultraviolet Tests/Graphics/Texture2DTests.cs
+++ TwistedLogik.Ultraviolet Tests/Graphics/Texture2DTests.cs
@@ -46,5 +46,37 @@ namespace TwistedLogik.Ultraviolet.Tests.Graphics
             TheResultingImage(result)
                 .ShouldMatch(@"Resources/Expected/Graphics/UltravioletGraphics_CanRenderNonPremultipliedTexture.png");
         }
+
+        [Test]
+        [Category("Rendering")]
+        [Description("Ensures that sprite fonts can be loaded and rendered correctly from preprocessed files.")]
+        public void Texture2D_LoadsAndRendersCorrectly_FromPreprocessedAsset()
+        {
+            var sbatch = default(SpriteBatch);
+            var texture = default(Texture2D);
+
+            var result = GivenAnUltravioletApplication()
+                .WithContent(content =>
+                {
+                    sbatch = SpriteBatch.Create();
+
+                    var textureAssetPath = CreateMachineSpecificAssetCopy(content, "Textures/Triangle_Preprocessed");
+                    if (!content.Preprocess<Texture2D>(textureAssetPath))
+                        Assert.Fail("Failed to preprocess asset.");
+
+                    texture = content.Load<Texture2D>(textureAssetPath + ".uvc");
+                })
+                .Render(uv =>
+                {
+                    uv.GetGraphics().Clear(Color.CornflowerBlue);
+
+                    sbatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+                    sbatch.Draw(texture, new RectangleF(0, 0, 256, 256), Color.White);
+                    sbatch.End();
+                });
+            
+            TheResultingImage(result)
+                .ShouldMatch(@"Resources/Expected/Graphics/Texture2D_LoadsAndRendersCorrectly_FromPreprocessedAsset.png");
+        }
     }
 }
