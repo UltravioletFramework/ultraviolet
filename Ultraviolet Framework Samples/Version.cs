cc113789e7ef5c22322950dc8958febfa34603f2
Cole Campbell <cole.campbell@twistedlogik.net>
2016-06-05 16:00:22 -0400

    Incremented version to 1.3.11

--- Ultraviolet Framework Samples/Version.cs
+++ Ultraviolet Framework Samples/Version.cs
@@ -5,5 +5,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Cole Campbell 2014-2016")]
 
 [assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.10.0")]
-[assembly: AssemblyInformationalVersion("1.3.10.0 dev build")]
+[assembly: AssemblyFileVersion("1.3.11.0")]
+[assembly: AssemblyInformationalVersion("1.3.11.0 dev build")]
