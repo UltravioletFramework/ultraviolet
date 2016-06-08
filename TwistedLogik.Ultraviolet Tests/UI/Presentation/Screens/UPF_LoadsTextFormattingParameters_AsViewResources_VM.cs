22c7b3acbce4c27c61348b03101c340ec41dabea
Cole Campbell <cole.campbell@twistedlogik.net>
2016-06-07 22:41:09 -0400

    Added test for UPF text formatting parameters

---
+++ TwistedLogik.Ultraviolet Tests/UI/Presentation/Screens/UPF_LoadsTextFormattingParameters_AsViewResources_VM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TwistedLogik.Ultraviolet.Graphics.Graphics2D;
+
+namespace TwistedLogik.Ultraviolet.Tests.UI.Presentation.Screens
+{
+    public class RainbowGlyphShader : GlyphShader
+    {
+        public override void Execute(ref GlyphShaderContext context, ref GlyphData data, Int32 index)
+        {
+            data.Color = Colors[index % Colors.Length];
+        }
+
+        private static readonly Color[] Colors = new[]
+        { Color.Red, Color.Blue, Color.Lime };
+    }
+
+    public class UPF_LoadsTextFormattingParameters_AsViewResources_VM
+    { }
+}
