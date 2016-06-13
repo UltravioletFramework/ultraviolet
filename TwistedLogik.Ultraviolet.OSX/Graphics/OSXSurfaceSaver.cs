e90f9b2f929c5abbda3a83ec9a9791acf64ce9fe
Cole Campbell <cole.campbell@twistedlogik.net>
2016-06-12 23:06:51 -0400

    Fix tabs/spaces

--- TwistedLogik.Ultraviolet.OSX/Graphics/OSXSurfaceSaver.cs
+++ TwistedLogik.Ultraviolet.OSX/Graphics/OSXSurfaceSaver.cs
@@ -9,117 +9,117 @@ using MonoMac.Foundation;
 
 namespace TwistedLogik.Ultraviolet.OSX
 {
-	/// <summary>
-	/// Represents an implementation of the <see cref="SurfaceSaver"/> class for Mac OS X.
-	/// </summary>
-	public sealed unsafe class OSXSurfaceSaver : SurfaceSaver
-	{
-		/// <inheritdoc/>
-		public override void SaveAsPng(Surface2D surface, Stream stream)
-		{
-			Contract.Require(surface, "surface");
-			Contract.Require(stream, "stream");
+    /// <summary>
+    /// Represents an implementation of the <see cref="SurfaceSaver"/> class for Mac OS X.
+    /// </summary>
+    public sealed unsafe class OSXSurfaceSaver : SurfaceSaver
+    {
+        /// <inheritdoc/>
+        public override void SaveAsPng(Surface2D surface, Stream stream)
+        {
+            Contract.Require(surface, "surface");
+            Contract.Require(stream, "stream");
 
-			Save(surface, stream, ImageFormat.Png);
-		}
+            Save(surface, stream, ImageFormat.Png);
+        }
 
-		/// <inheritdoc/>
-		public override void SaveAsJpeg(Surface2D surface, Stream stream)
-		{
-			Contract.Require(surface, "surface");
-			Contract.Require(stream, "stream");
+        /// <inheritdoc/>
+        public override void SaveAsJpeg(Surface2D surface, Stream stream)
+        {
+            Contract.Require(surface, "surface");
+            Contract.Require(stream, "stream");
 
-			Save(surface, stream, ImageFormat.Jpeg);
-		}
+            Save(surface, stream, ImageFormat.Jpeg);
+        }
 
-		/// <inheritdoc/>
-		public override void SaveAsPng(RenderTarget2D renderTarget, Stream stream)
-		{
-			Contract.Require(renderTarget, "renderTarget");
-			Contract.Require(stream, "stream");
+        /// <inheritdoc/>
+        public override void SaveAsPng(RenderTarget2D renderTarget, Stream stream)
+        {
+            Contract.Require(renderTarget, "renderTarget");
+            Contract.Require(stream, "stream");
 
-			Save(renderTarget, stream, ImageFormat.Png);
-		}
+            Save(renderTarget, stream, ImageFormat.Png);
+        }
 
-		/// <inheritdoc/>
-		public override void SaveAsJpeg(RenderTarget2D renderTarget, Stream stream)
-		{
-			Contract.Require(renderTarget, "renderTarget");
-			Contract.Require(stream, "stream");
+        /// <inheritdoc/>
+        public override void SaveAsJpeg(RenderTarget2D renderTarget, Stream stream)
+        {
+            Contract.Require(renderTarget, "renderTarget");
+            Contract.Require(stream, "stream");
 
-			Save(renderTarget, stream, ImageFormat.Jpeg);
-		}
+            Save(renderTarget, stream, ImageFormat.Jpeg);
+        }
 
-		/// <summary>
-		/// Saves the specified surface as an image with the specified format.
-		/// </summary>
-		/// <param name="surface">The surface to save.</param>
-		/// <param name="stream">The stream to which to save the surface data.</param>
-		/// <param name="format">The format with which to save the image.</param>
-		private void Save(Surface2D surface, Stream stream, ImageFormat format)
-		{
-			var data = new Color[surface.Width * surface.Height];
-			surface.GetData(data);
+        /// <summary>
+        /// Saves the specified surface as an image with the specified format.
+        /// </summary>
+        /// <param name="surface">The surface to save.</param>
+        /// <param name="stream">The stream to which to save the surface data.</param>
+        /// <param name="format">The format with which to save the image.</param>
+        private void Save(Surface2D surface, Stream stream, ImageFormat format)
+        {
+            var data = new Color[surface.Width * surface.Height];
+            surface.GetData(data);
 
-			Save(data, surface.Width, surface.Height, stream, format);
-		}
+            Save(data, surface.Width, surface.Height, stream, format);
+        }
 
-		/// <summary>
-		/// Saves the specified render target as an image with the specified format.
-		/// </summary>
-		/// <param name="renderTarget">The render target to save.</param>
-		/// <param name="stream">The stream to which to save the render target data.</param>
-		/// <param name="format">The format with which to save the image.</param>
-		private void Save(RenderTarget2D renderTarget, Stream stream, ImageFormat format)
-		{
-			var data = new Color[renderTarget.Width * renderTarget.Height];
-			renderTarget.GetData(data);
+        /// <summary>
+        /// Saves the specified render target as an image with the specified format.
+        /// </summary>
+        /// <param name="renderTarget">The render target to save.</param>
+        /// <param name="stream">The stream to which to save the render target data.</param>
+        /// <param name="format">The format with which to save the image.</param>
+        private void Save(RenderTarget2D renderTarget, Stream stream, ImageFormat format)
+        {
+            var data = new Color[renderTarget.Width * renderTarget.Height];
+            renderTarget.GetData(data);
 
-			Save(data, renderTarget.Width, renderTarget.Height, stream, format);
-		}
+            Save(data, renderTarget.Width, renderTarget.Height, stream, format);
+        }
 
-		/// <summary>
-		/// Saves the specified color data as an image with the specified format.
-		/// </summary>
-		/// <param name="data">An array containing the image's color data.</param>
-		/// <param name="width">The width of the image in pixels.</param>
-		/// <param name="height">The height of the image in pixels.</param>
-		/// <param name="stream">The stream to which to save the image data.</param>
-		/// <param name="format">The format with which to save the image.</param>
-		private void Save(Color[] data, Int32 width, Int32 height, Stream stream, ImageFormat format)
-		{	
-			using (var rep = new NSBitmapImageRep(IntPtr.Zero, width, height, 8, 4, true, false, "NSCalibratedRGBColorSpace", 0, 0)) 
-			{
-				fixed (Color* pData = data)
-				{
-					for (int y = 0; y < height; y++)
-					{
-						var pSrc = pData + (y * width);
-						var pDst = (Byte*)rep.BitmapData + (y * rep.BytesPerRow);
+        /// <summary>
+        /// Saves the specified color data as an image with the specified format.
+        /// </summary>
+        /// <param name="data">An array containing the image's color data.</param>
+        /// <param name="width">The width of the image in pixels.</param>
+        /// <param name="height">The height of the image in pixels.</param>
+        /// <param name="stream">The stream to which to save the image data.</param>
+        /// <param name="format">The format with which to save the image.</param>
+        private void Save(Color[] data, Int32 width, Int32 height, Stream stream, ImageFormat format)
+        {	
+            using (var rep = new NSBitmapImageRep(IntPtr.Zero, width, height, 8, 4, true, false, "NSCalibratedRGBColorSpace", 0, 0)) 
+            {
+                fixed (Color* pData = data)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        var pSrc = pData + (y * width);
+                        var pDst = (Byte*)rep.BitmapData + (y * rep.BytesPerRow);
 
-						for (int x = 0; x < width; x++)
-						{
+                        for (int x = 0; x < width; x++)
+                        {
                             var color = *pSrc++;
-							*pDst++ = color.R;
-							*pDst++ = color.G;
-							*pDst++ = color.B;
-							*pDst++ = color.A;
-						}
-					}
-				}
+                            *pDst++ = color.R;
+                            *pDst++ = color.G;
+                            *pDst++ = color.B;
+                            *pDst++ = color.A;
+                        }
+                    }
+                }
 
-				var filetype = (format == ImageFormat.Png) ? NSBitmapImageFileType.Png : NSBitmapImageFileType.Jpeg;
-				var properties = new NSDictionary();
+                var filetype = (format == ImageFormat.Png) ? NSBitmapImageFileType.Png : NSBitmapImageFileType.Jpeg;
+                var properties = new NSDictionary();
 
-				using (var imgData = rep.RepresentationUsingTypeProperties(filetype, properties))
-				{
-					using (var imgStream = imgData.AsStream())
-					{
-						imgStream.CopyTo(stream);
-					}
-				}
-			}
-		}
-	}
+                using (var imgData = rep.RepresentationUsingTypeProperties(filetype, properties))
+                {
+                    using (var imgStream = imgData.AsStream())
+                    {
+                        imgStream.CopyTo(stream);
+                    }
+                }
+            }
+        }
+    }
 }
 
--- TwistedLogik.Ultraviolet.OSX/Graphics/OSXSurfaceSource.cs
+++ TwistedLogik.Ultraviolet.OSX/Graphics/OSXSurfaceSource.cs
@@ -7,129 +7,129 @@ using TwistedLogik.Ultraviolet.Graphics;
 
 namespace TwistedLogik.Ultraviolet.OSX
 {
-	/// <summary>
-	/// Represents an implementation of the <see cref="SurfaceSource"/> class for Mac OS X.
-	/// </summary>
-	public sealed unsafe class OSXSurfaceSource : SurfaceSource
-	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="OSXSurfaceSource"/> class from the specified stream.
-		/// </summary>
-		/// <param name="stream">The <see cref="Stream"/> that contains the surface data.</param>
-		public OSXSurfaceSource(Stream stream)
-		{
-			Contract.Require(stream, "stream");
+    /// <summary>
+    /// Represents an implementation of the <see cref="SurfaceSource"/> class for Mac OS X.
+    /// </summary>
+    public sealed unsafe class OSXSurfaceSource : SurfaceSource
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OSXSurfaceSource"/> class from the specified stream.
+        /// </summary>
+        /// <param name="stream">The <see cref="Stream"/> that contains the surface data.</param>
+        public OSXSurfaceSource(Stream stream)
+        {
+            Contract.Require(stream, "stream");
 
-			var data = new Byte[stream.Length];
-			stream.Read(data, 0, data.Length);
+            var data = new Byte[stream.Length];
+            stream.Read(data, 0, data.Length);
 
-			using (var mstream = new MemoryStream(data))
-			{
-				this.image = NSImage.FromStream(mstream);
-				this.imageRep = new NSBitmapImageRep(image.CGImage);
-			}
-		}
+            using (var mstream = new MemoryStream(data))
+            {
+                this.image = NSImage.FromStream(mstream);
+                this.imageRep = new NSBitmapImageRep(image.CGImage);
+            }
+        }
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="OSXSurfaceSource"/> class from the specified image.
-		/// </summary>
-		/// <param name="stream">The <see cref="NSImage"/> that contains the surface data.</param>
-		public OSXSurfaceSource(NSImage image)
-		{
-			Contract.Require(image, "image");
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OSXSurfaceSource"/> class from the specified image.
+        /// </summary>
+        /// <param name="stream">The <see cref="NSImage"/> that contains the surface data.</param>
+        public OSXSurfaceSource(NSImage image)
+        {
+            Contract.Require(image, "image");
 
-			this.image = image;
-			this.imageRep = new NSBitmapImageRep(image.CGImage);
-		}
+            this.image = image;
+            this.imageRep = new NSBitmapImageRep(image.CGImage);
+        }
 
-		/// <inheritdoc/>
-		public override void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
+        /// <inheritdoc/>
+        public override void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
-		/// <inheritdoc/>
-		public override Color this[int x, int y]
-		{
-			get
-			{
-				var pixel = ((byte*)imageRep.BitmapData) + (imageRep.BytesPerRow * y) + (x * sizeof(UInt32));
-				var a = *pixel++;
-				var r = *pixel++;
-				var g = *pixel++;
-				var b = *pixel++;
-				return new Color(r, g, b, a);
-			}
-		}
+        /// <inheritdoc/>
+        public override Color this[int x, int y]
+        {
+            get
+            {
+                var pixel = ((byte*)imageRep.BitmapData) + (imageRep.BytesPerRow * y) + (x * sizeof(UInt32));
+                var a = *pixel++;
+                var r = *pixel++;
+                var g = *pixel++;
+                var b = *pixel++;
+                return new Color(r, g, b, a);
+            }
+        }
 
-		/// <inheritdoc/>
-		public override IntPtr Data
-		{
-			get
-			{
-				return this.imageRep.BitmapData;
-			}
-		}
+        /// <inheritdoc/>
+        public override IntPtr Data
+        {
+            get
+            {
+                return this.imageRep.BitmapData;
+            }
+        }
 
-		/// <inheritdoc/>
-		public override Int32 Stride
-		{
-			get
-			{
-				return this.imageRep.BytesPerRow;
-			}
-		}
+        /// <inheritdoc/>
+        public override Int32 Stride
+        {
+            get
+            {
+                return this.imageRep.BytesPerRow;
+            }
+        }
 
-		/// <inheritdoc/>
-		public override Int32 Width
-		{
-			get
-			{
-				return this.imageRep.PixelsWide;
-			}
-		}
+        /// <inheritdoc/>
+        public override Int32 Width
+        {
+            get
+            {
+                return this.imageRep.PixelsWide;
+            }
+        }
 
-		/// <inheritdoc/>
-		public override Int32 Height
-		{
-			get
-			{
-				return this.imageRep.PixelsHigh;
-			}
-		}
+        /// <inheritdoc/>
+        public override Int32 Height
+        {
+            get
+            {
+                return this.imageRep.PixelsHigh;
+            }
+        }
 
-		/// <inheritdoc/>
-		public override SurfaceSourceDataFormat DataFormat
-		{
-			get
-			{
-				return SurfaceSourceDataFormat.BGRA;
-			}
-		}
+        /// <inheritdoc/>
+        public override SurfaceSourceDataFormat DataFormat
+        {
+            get
+            {
+                return SurfaceSourceDataFormat.BGRA;
+            }
+        }
 
-		/// <summary>
-		/// Releases resources associated with the object.
-		/// </summary>
-		/// <param name="disposing"><c>true</c> if the object is being disposed; <c>false</c> if the object is being finalized.</param>
-		private void Dispose(Boolean disposing)
-		{
-			if (disposed)
-				return;
+        /// <summary>
+        /// Releases resources associated with the object.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> if the object is being disposed; <c>false</c> if the object is being finalized.</param>
+        private void Dispose(Boolean disposing)
+        {
+            if (disposed)
+                return;
 
-			if (disposing)
-			{
-				this.imageRep.Dispose();
-				this.image.Dispose();
-			}
+            if (disposing)
+            {
+                this.imageRep.Dispose();
+                this.image.Dispose();
+            }
 
-			disposed = true;
-		}
+            disposed = true;
+        }
 
-		// State values.
-		private readonly NSImage image;
-		private readonly NSBitmapImageRep imageRep;
-		private Boolean disposed;
-	}
+        // State values.
+        private readonly NSImage image;
+        private readonly NSBitmapImageRep imageRep;
+        private Boolean disposed;
+    }
 }
 
