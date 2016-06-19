5e29e224d6003bfd46d63fb3795b008d1d2569bb
Cole Campbell <cole.campbell@twistedlogik.net>
2016-06-19 09:58:53 -0400

    Fix BASSFX to refer to __Internal on iOS

--- TwistedLogik.Ultraviolet.BASS/Native/BASSFXNative.cs
+++ TwistedLogik.Ultraviolet.BASS/Native/BASSFXNative.cs
@@ -8,15 +8,21 @@ namespace TwistedLogik.Ultraviolet.BASS.Native
     [SuppressUnmanagedCodeSecurity]
     internal static class BASSFXNative
     {
+#if IOS
+        const String LibraryPath = "__Internal";
+#else
+        const String LibraryPath = "bass_fx";
+#endif
+
         static BASSFXNative()
         {
             LibraryLoader.Load("bass_fx");
         }
 
-        [DllImport("bass_fx", EntryPoint = "BASS_FX_GetVersion")]
+        [DllImport(LibraryPath, EntryPoint = "BASS_FX_GetVersion")]
         public static extern UInt32 GetVersion();
 
-        [DllImport("bass_fx", EntryPoint = "BASS_FX_TempoCreate")]
+        [DllImport(LibraryPath, EntryPoint = "BASS_FX_TempoCreate")]
         public static extern UInt32 TempoCreate(UInt32 chan, UInt32 flags);
     }
 }
