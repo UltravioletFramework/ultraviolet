3e48361a3750a48f98652133839c9406a9a988e2
Cole Campbell <cole.campbell@twistedlogik.net>
2016-07-22 21:54:31 -0400

    Add more tests around Grid

---
+++ TwistedLogik.Ultraviolet Tests/UI/Presentation/Screens/UPF_Grid_ArrangesAutoCellsCorrectly.cs
@@ -0,0 +1,13 @@
+﻿using TwistedLogik.Ultraviolet.Content;
+
+namespace TwistedLogik.Ultraviolet.Tests.UI.Presentation.Screens
+{
+    public class UPF_Grid_ArrangesAutoCellsCorrectly : TestScreenBase<UPF_Grid_ArrangesAutoCellsCorrectly_VM>
+    {
+        public UPF_Grid_ArrangesAutoCellsCorrectly(ContentManager globalContent)
+            : base("Content/UI/Screens/UPF_Grid_ArrangesAutoCellsCorrectly", "View", globalContent)
+        {
+
+        }
+    }
+}
