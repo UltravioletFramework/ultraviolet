c8fe405ad6a393dc6368de759090237446f37a3d
Cole Campbell <cole.campbell@twistedlogik.net>
2017-05-16 15:33:42 -0400

    Move SDL-specific context code into base class

--- Source/Ultraviolet.OpenGL/Shared/OpenGLUltravioletContext.cs
+++ Source/Ultraviolet.OpenGL/Shared/OpenGLUltravioletContext.cs
@@ -2,12 +2,10 @@
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices;
 using Ultraviolet.Content;
 using Ultraviolet.Core;
 using Ultraviolet.Graphics;
 using Ultraviolet.SDL2;
-using Ultraviolet.SDL2.Messages;
 using Ultraviolet.SDL2.Native;
 using Ultraviolet.SDL2.Platform;
 using Ultraviolet.UI;
@@ -18,7 +16,7 @@ namespace Ultraviolet.OpenGL
     /// Represents the OpenGL implementation of the Ultraviolet context.
     /// </summary>
     [CLSCompliant(true)]
-    public sealed class OpenGLUltravioletContext : UltravioletContext
+    public sealed class OpenGLUltravioletContext : SDL2UltravioletContext
     {
         /// <summary>
         /// Initializes a new instance of the OpenGLUltravioletContext class.
@@ -40,87 +38,18 @@ namespace Ultraviolet.OpenGL
         {
             Contract.Require(configuration, nameof(configuration));
 
-            this.IsHardwareInputDisabled = configuration.IsHardwareInputDisabled;
+            IsHardwareInputDisabled = configuration.IsHardwareInputDisabled;
 
-            var sdlFlags = configuration.EnableServiceMode ?
-                SDL_Init.TIMER | SDL_Init.EVENTS :
-                SDL_Init.TIMER | SDL_Init.VIDEO | SDL_Init.JOYSTICK | SDL_Init.GAMECONTROLLER | SDL_Init.EVENTS;
-
-            if (Platform == UltravioletPlatform.Windows)
-                SDL.SetHint(SDL_Hint.HINT_WINDOWS_DISABLE_THREAD_NAMING, "1");
-
-            if (SDL.Init(sdlFlags) != 0)
+            if (!InitSDL(configuration))
                 throw new SDL2Exception();
 
-            var isGLES = (Platform == UltravioletPlatform.Android || Platform == UltravioletPlatform.iOS);
-
-            var versionRequired = isGLES ? new Version(2, 0) : new Version(3, 1);
-            var versionRequested = isGLES ? configuration.MinimumOpenGLESVersion : configuration.MinimumOpenGLVersion;
-            if (versionRequested == null || versionRequested < versionRequired)
-            {
-                if (isGLES)
-                {
-                    versionRequested = Platform == UltravioletPlatform.Android ?
-                        new Version(2, 0) : new Version(3, 0);
-                }
-                else
-                {
-                    versionRequested = versionRequired;
-                }
-            }
+            var isGLEs = false;
+            var versionRequested = default(Version);
+            var versionRequired = default(Version);
+            InitOpenGLVersion(configuration, out versionRequested, out versionRequired, out isGLEs);
 
             if (!configuration.EnableServiceMode)
-            {
-                var profile = isGLES ? SDL_GLprofile.ES : SDL_GLprofile.CORE;
-
-                if (SDL.GL_SetAttribute(SDL_GLattr.CONTEXT_PROFILE_MASK, (Int32)profile) < 0)
-                    throw new SDL2Exception();
-
-                // NOTE: Asking for an ES 3.0 context in the emulator will return a valid
-                // context pointer, but actually using it will cause segfaults. It seems like
-                // the best thing to do on Android is just not ask for a specific version,
-                // and trust the OS to give you the highest version it supports.
-                if (Platform != UltravioletPlatform.Android)
-                {
-                    if (SDL.GL_SetAttribute(SDL_GLattr.CONTEXT_MAJOR_VERSION, versionRequested.Major) < 0)
-                        throw new SDL2Exception();
-
-                    if (SDL.GL_SetAttribute(SDL_GLattr.CONTEXT_MINOR_VERSION, versionRequested.Minor) < 0)
-                        throw new SDL2Exception();
-                }
-
-                if (SDL.GL_SetAttribute(SDL_GLattr.DEPTH_SIZE, configuration.BackBufferDepthSize) < 0)
-                    throw new SDL2Exception();
-
-                if (SDL.GL_SetAttribute(SDL_GLattr.STENCIL_SIZE, configuration.BackBufferStencilSize) < 0)
-                    throw new SDL2Exception();
-
-                if (SDL.GL_SetAttribute(SDL_GLattr.RETAINED_BACKING, 0) < 0)
-                    throw new SDL2Exception();
-
-                if (configuration.Use32BitFramebuffer)
-                {
-                    if (SDL.GL_SetAttribute(SDL_GLattr.RED_SIZE, 8) < 0)
-                        throw new SDL2Exception();
-
-                    if (SDL.GL_SetAttribute(SDL_GLattr.GREEN_SIZE, 8) < 0)
-                        throw new SDL2Exception();
-
-                    if (SDL.GL_SetAttribute(SDL_GLattr.BLUE_SIZE, 8) < 0)
-                        throw new SDL2Exception();
-                }
-                else
-                {
-                    if (SDL.GL_SetAttribute(SDL_GLattr.RED_SIZE, 5) < 0)
-                        throw new SDL2Exception();
-
-                    if (SDL.GL_SetAttribute(SDL_GLattr.GREEN_SIZE, 6) < 0)
-                        throw new SDL2Exception();
-
-                    if (SDL.GL_SetAttribute(SDL_GLattr.BLUE_SIZE, 5) < 0)
-                        throw new SDL2Exception();
-                }
-            }
+                InitOpenGLAttributes(configuration, versionRequested, versionRequired, isGLEs);
 
             var sdlAssembly = typeof(SDL).Assembly;
             InitializeFactoryMethodsInAssembly(sdlAssembly);
@@ -133,13 +62,19 @@ namespace Ultraviolet.OpenGL
 
             PumpEvents();
 
-            this.graphics = IsRunningInServiceMode ? (IUltravioletGraphics)new DummyUltravioletGraphics(this) : new OpenGLUltravioletGraphics(this, configuration, versionRequested);
-            if (!IsRunningInServiceMode)
-                ((OpenGLUltravioletGraphics)graphics).ResetDeviceStates();
-
-            this.audio = IsRunningInServiceMode ? new DummyUltravioletAudio(this) : InitializeAudioSubsystem(configuration);
-
-            this.input = IsRunningInServiceMode ? (IUltravioletInput)(new DummyUltravioletInput(this)) : new SDL2UltravioletInput(this);
+            if (IsRunningInServiceMode)
+            {
+                this.graphics = new DummyUltravioletGraphics(this);
+                this.audio = new DummyUltravioletAudio(this);
+                this.input = new DummyUltravioletInput(this);
+            }
+            else
+            {
+                this.graphics = new OpenGLUltravioletGraphics(this, configuration, versionRequested);
+                ((OpenGLUltravioletGraphics)this.graphics).ResetDeviceStates();
+                this.audio = InitializeAudioSubsystem(configuration);
+                this.input = new SDL2UltravioletInput(this);
+            }
 
             this.content = new UltravioletContent(this);
             this.content.RegisterImportersAndProcessors(new[] { sdlAssembly, AudioSubsystemAssembly });
@@ -149,61 +84,8 @@ namespace Ultraviolet.OpenGL
 
             PumpEvents();
             
-            unsafe
-            {
-                eventFilter = new SDL.EventFilter(SDLEventFilter);
-                eventFilterPtr = Marshal.GetFunctionPointerForDelegate(eventFilter);
-                SDL.SetEventFilter(eventFilterPtr, IntPtr.Zero);
-            }
-
             InitializeContext();
             InitializeViewProvider(configuration);
-        }
-
-        /// <inheritdoc/>
-        public override void UpdateSuspended()
-        {
-            SDL.PumpEvents();
-
-            base.UpdateSuspended();
-        }
-
-        /// <inheritdoc/>
-        public override void Update(UltravioletTime time)
-        {
-            Contract.Require(time, nameof(time));
-            Contract.EnsureNotDisposed(this, Disposed);
-
-            var sdlinput = input as SDL2UltravioletInput;
-            if (sdlinput != null)
-                sdlinput.ResetDeviceStates();
-
-            if (!PumpEvents())
-            {
-                return;
-            }
-
-            ProcessMessages();
-
-            OnUpdatingSubsystems(time);
-
-            platform.Update(time);
-            content.Update(time);
-
-            if (!IsRunningInServiceMode)
-            {
-                graphics.Update(time);
-            }
-
-            audio.Update(time);
-            input.Update(time);
-            ui.Update(time);
-
-            ProcessMessages();
-
-            OnUpdating(time);
-
-            UpdateContext(time);
         }
 
         /// <inheritdoc/>
@@ -217,8 +99,8 @@ namespace Ultraviolet.OpenGL
             if (oglgfx != null)
             {
                 var glcontext = oglgfx.OpenGLContext;
-                var windowInfo = ((SDL2UltravioletWindowInfo)platform.Windows);
-                foreach (SDL2UltravioletWindow window in windowInfo)
+                var windowInfo = (SDL2UltravioletWindowInfo)platform.Windows;
+                foreach (SDL2UltravioletWindow window in platform.Windows)
                 {
                     windowInfo.DesignateCurrent(window, glcontext);
 
@@ -307,56 +189,6 @@ namespace Ultraviolet.OpenGL
             }
         }
 
-        /// <inheritdoc/>
-        protected override void OnShutdown()
-        {
-            SDL.SetEventFilter(IntPtr.Zero, IntPtr.Zero);
-            SDL.Quit();
-
-            base.OnShutdown();
-        }
-
-        /// <summary>
-        /// Filters SDL2 events.
-        /// </summary>
-        [MonoPInvokeCallback(typeof(SDL.EventFilter))]
-        private static unsafe Int32 SDLEventFilter(IntPtr userdata, SDL_Event* @event)
-        {
-            var uv = RequestCurrent();
-            if (uv == null)
-                return 1;
-
-            switch (@event->type)
-            {
-                case SDL_EventType.APP_TERMINATING:
-                    uv.Messages.PublishImmediate(UltravioletMessages.ApplicationTerminating, null);
-                    return 0;
-
-                case SDL_EventType.APP_WILLENTERBACKGROUND:
-                    uv.Messages.PublishImmediate(UltravioletMessages.ApplicationSuspending, null);
-                    return 0;
-
-                case SDL_EventType.APP_DIDENTERBACKGROUND:
-                    uv.Messages.PublishImmediate(UltravioletMessages.ApplicationSuspended, null);
-                    return 0;
-
-                case SDL_EventType.APP_WILLENTERFOREGROUND:
-                    uv.Messages.PublishImmediate(UltravioletMessages.ApplicationResuming, null);
-                    return 0;
-
-                case SDL_EventType.APP_DIDENTERFOREGROUND:
-                    uv.Messages.PublishImmediate(UltravioletMessages.ApplicationResumed, null);
-                    return 0;
-
-                case SDL_EventType.APP_LOWMEMORY:
-                    uv.Messages.PublishImmediate(UltravioletMessages.LowMemory, null);
-                    GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
-                    return 0;
-            }
-
-            return 1;
-        }
-
         /// <summary>
         /// Initializes the context's audio subsystem.
         /// </summary>
@@ -412,62 +244,84 @@ namespace Ultraviolet.OpenGL
         }
 
         /// <summary>
-        /// Pumps the SDL2 event queue.
+        /// Determines which version of OpenGL will be used by the context.
         /// </summary>
-        /// <returns><see langword="true"/> if the context should continue processing the frame; otherwise, <see langword="false"/>.</returns>
-        private Boolean PumpEvents()
+        private void InitOpenGLVersion(OpenGLUltravioletConfiguration configuration,
+            out Version versionRequested, out Version versionRequired, out Boolean isGLES)
         {
-            SDL_Event @event;
-            while (SDL.PollEvent(out @event) > 0)
+            isGLES = (Platform == UltravioletPlatform.Android || Platform == UltravioletPlatform.iOS);
+
+            versionRequired = isGLES ? new Version(2, 0) : new Version(3, 1);
+            versionRequested = isGLES ? configuration.MinimumOpenGLESVersion : configuration.MinimumOpenGLVersion;
+            if (versionRequested == null || versionRequested < versionRequired)
             {
-                if (Disposed)
-                    return false;
-
-                switch (@event.type)
+                if (isGLES)
                 {
-                    case SDL_EventType.WINDOWEVENT:
-                        if (@event.window.@event == SDL_WindowEventID.CLOSE)
-                        {
-                            var glWindowInfo = (SDL2UltravioletWindowInfo)GetPlatform().Windows;
-                            if (glWindowInfo.DestroyByID((int)@event.window.windowID))
-                            {
-                                Messages.Publish(UltravioletMessages.Quit, null);
-                                return true;
-                            }
-                        }
-                        break;
-
-                    case SDL_EventType.KEYDOWN:
-                    case SDL_EventType.KEYUP:
-                    case SDL_EventType.MOUSEBUTTONDOWN:
-                    case SDL_EventType.MOUSEBUTTONUP:
-                    case SDL_EventType.MOUSEMOTION:
-                    case SDL_EventType.MOUSEWHEEL:
-                    case SDL_EventType.JOYAXISMOTION:
-                    case SDL_EventType.JOYBALLMOTION:
-                    case SDL_EventType.JOYBUTTONDOWN:
-                    case SDL_EventType.JOYBUTTONUP:
-                    case SDL_EventType.JOYHATMOTION:
-                    case SDL_EventType.CONTROLLERAXISMOTION:
-                    case SDL_EventType.CONTROLLERBUTTONDOWN:
-                    case SDL_EventType.CONTROLLERBUTTONUP:
-                        if (IsHardwareInputDisabled)
-                        {
-                            continue;
-                        }
-                        break;
-
-                    case SDL_EventType.QUIT:
-                        Messages.Publish(UltravioletMessages.Quit, null);
-                        return true;
+                    versionRequested = Platform == UltravioletPlatform.Android ?
+                        new Version(2, 0) : new Version(3, 0);
                 }
-
-                // Publish any SDL events to the message queue.
-                var data = Messages.CreateMessageData<SDL2EventMessageData>();
-                data.Event = @event;
-                Messages.Publish(SDL2UltravioletMessages.SDLEvent, data);
+                else
+                {
+                    versionRequested = versionRequired;
+                }
             }
-            return !Disposed;
+        }
+
+        /// <summary>
+        /// Sets the SDL2 attributes which correspond to the application's OpenGL settings.
+        /// </summary>
+        private void InitOpenGLAttributes(OpenGLUltravioletConfiguration configuration, 
+            Version versionRequested, Version versionRequired, Boolean isGLES)
+        {
+            var profile = isGLES ? SDL_GLprofile.ES : SDL_GLprofile.CORE;
+
+            if (SDL.GL_SetAttribute(SDL_GLattr.CONTEXT_PROFILE_MASK, (Int32)profile) < 0)
+                throw new SDL2Exception();
+
+            // NOTE: Asking for an ES 3.0 context in the emulator will return a valid
+            // context pointer, but actually using it will cause segfaults. It seems like
+            // the best thing to do on Android is just not ask for a specific version,
+            // and trust the OS to give you the highest version it supports.
+            if (Platform != UltravioletPlatform.Android)
+            {
+                if (SDL.GL_SetAttribute(SDL_GLattr.CONTEXT_MAJOR_VERSION, versionRequested.Major) < 0)
+                    throw new SDL2Exception();
+
+                if (SDL.GL_SetAttribute(SDL_GLattr.CONTEXT_MINOR_VERSION, versionRequested.Minor) < 0)
+                    throw new SDL2Exception();
+            }
+
+            if (SDL.GL_SetAttribute(SDL_GLattr.DEPTH_SIZE, configuration.BackBufferDepthSize) < 0)
+                throw new SDL2Exception();
+
+            if (SDL.GL_SetAttribute(SDL_GLattr.STENCIL_SIZE, configuration.BackBufferStencilSize) < 0)
+                throw new SDL2Exception();
+
+            if (SDL.GL_SetAttribute(SDL_GLattr.RETAINED_BACKING, 0) < 0)
+                throw new SDL2Exception();
+
+            if (configuration.Use32BitFramebuffer)
+            {
+                if (SDL.GL_SetAttribute(SDL_GLattr.RED_SIZE, 8) < 0)
+                    throw new SDL2Exception();
+
+                if (SDL.GL_SetAttribute(SDL_GLattr.GREEN_SIZE, 8) < 0)
+                    throw new SDL2Exception();
+
+                if (SDL.GL_SetAttribute(SDL_GLattr.BLUE_SIZE, 8) < 0)
+                    throw new SDL2Exception();
+            }
+            else
+            {
+                if (SDL.GL_SetAttribute(SDL_GLattr.RED_SIZE, 5) < 0)
+                    throw new SDL2Exception();
+
+                if (SDL.GL_SetAttribute(SDL_GLattr.GREEN_SIZE, 6) < 0)
+                    throw new SDL2Exception();
+
+                if (SDL.GL_SetAttribute(SDL_GLattr.BLUE_SIZE, 5) < 0)
+                    throw new SDL2Exception();
+            }
         }
 
         // Ultraviolet subsystems.
@@ -477,10 +331,6 @@ namespace Ultraviolet.OpenGL
         private readonly IUltravioletAudio audio;
         private readonly IUltravioletInput input;
         private readonly IUltravioletUI ui;
-
-        // The SDL event filter.
-        private readonly SDL.EventFilter eventFilter;
-        private readonly IntPtr eventFilterPtr;
 
         // Property values.
         private Assembly audioSubsystemAssembly;
