8c5079cece5123b5fd110717dba8d36f0cd46f49
Cole Campbell <cole.campbell@twistedlogik.net>
2017-12-26 00:20:09 -0500

    Revert "Fix Windows Forms package names"
    This reverts commit d568880448eacc382c26ba5fdb62091f4f1d0473.

---
+++ Packages/Ultraviolet.Game.WindowsForms/content/Form1.Designer.cs.pp
@@ -0,0 +1,131 @@
+namespace $RootNamespace$
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fILEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.ultravioletPanel2 = new TwistedLogik.Ultraviolet.WindowsForms.UltravioletPanel();
+            this.ultravioletPanel1 = new TwistedLogik.Ultraviolet.WindowsForms.UltravioletPanel();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fILEToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fILEToolStripMenuItem
+            // 
+            this.fILEToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fILEToolStripMenuItem.Name = "fILEToolStripMenuItem";
+            this.fILEToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fILEToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.ultravioletPanel2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.ultravioletPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(784, 537);
+            this.splitContainer1.SplitterDistance = 261;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // ultravioletPanel2
+            // 
+            this.ultravioletPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ultravioletPanel2.Location = new System.Drawing.Point(55, 55);
+            this.ultravioletPanel2.Name = "ultravioletPanel2";
+            this.ultravioletPanel2.Size = new System.Drawing.Size(150, 150);
+            this.ultravioletPanel2.TabIndex = 0;
+            this.ultravioletPanel2.Drawing += new System.EventHandler(this.ultravioletPanel2_Drawing);
+            // 
+            // ultravioletPanel1
+            // 
+            this.ultravioletPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ultravioletPanel1.Location = new System.Drawing.Point(0, 0);
+            this.ultravioletPanel1.Name = "ultravioletPanel1";
+            this.ultravioletPanel1.Size = new System.Drawing.Size(519, 537);
+            this.ultravioletPanel1.TabIndex = 0;
+            this.ultravioletPanel1.Drawing += new System.EventHandler(this.ultravioletPanel1_Drawing);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Ultraviolet Form";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fILEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private TwistedLogik.Ultraviolet.WindowsForms.UltravioletPanel ultravioletPanel1;
+        private TwistedLogik.Ultraviolet.WindowsForms.UltravioletPanel ultravioletPanel2;
+    }
+}
---
+++ Packages/Ultraviolet.Game.WindowsForms/content/Form1.cs.pp
@@ -0,0 +1,93 @@
+using System;
+using TwistedLogik.Nucleus;
+using TwistedLogik.Ultraviolet;
+using TwistedLogik.Ultraviolet.Content;
+using TwistedLogik.Ultraviolet.OpenGL;
+using TwistedLogik.Ultraviolet.WindowsForms;
+
+namespace $RootNamespace$
+{
+    /// <summary>
+    /// The primary form in an Ultraviolet application.
+    /// </summary>
+    public partial class Form1 : UltravioletForm
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <inheritdoc/>
+        protected override UltravioletContext OnCreatingUltravioletContext(out UltravioletTickMode tickmode)
+        {
+            // NOTE: Making the context headless allows Windows Forms to control window creation for us.
+            tickmode = UltravioletTickMode.Idle;
+            return new OpenGLUltravioletContext(this, new OpenGLUltravioletConfiguration()
+            {
+                Headless = true
+            });
+        }
+
+        /// <inheritdoc/>
+        protected override void OnLoadingContent()
+        {
+            this.content = ContentManager.Create("Content");
+
+            // TODO: Load content here
+
+            base.OnLoadingContent();
+        }
+
+        /// <inheritdoc/>
+        protected override void OnUpdating(UltravioletTime time)
+        {
+            // TODO: Update the game state
+
+            base.OnUpdating(time);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnClosed(EventArgs e)
+        {
+            SafeDispose.DisposeRef(ref content);
+
+            base.OnClosed(e);
+        }
+
+        /// <summary>
+        /// Handles the Click event for exitToolStripMenuItem.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">An EventArgs that contains the event data.</param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Handles the Drawing event for ultravioletPanel1.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">An EventArgs that contains the event data.</param>
+        private void ultravioletPanel1_Drawing(object sender, EventArgs e)
+        {
+            // TODO: Draw the first panel
+        }
+
+        /// <summary>
+        /// Handles the Drawing event for ultravioletPanel2.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">An EventArgs that contains the event data.</param>
+        private void ultravioletPanel2_Drawing(object sender, EventArgs e)
+        {
+            // TODO: Draw the second panel
+        }
+
+        // The global content manager.  Manages any content that should remain loaded for the duration of the application's execution.
+        private ContentManager content;
+    }
+}
