e6242b5cbd51c71f9c15a5d89fce057c091b6386
Cole Campbell <cole.campbell@twistedlogik.net>
2017-12-25 20:55:57 -0500

    Don't include release name in version info

--- Source/Version.cs
+++ Source/Version.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("Ultraviolet Framework")]
-[assembly: AssemblyCopyright("Copyright © Cole Campbell 2014-2017")]
+[assembly: AssemblyCopyright("Copyright (c) Cole Campbell 2014-2017")]
 
 [assembly: AssemblyVersion("2017.12.0.0")]
 [assembly: AssemblyFileVersion("2017.12.0.0")]
