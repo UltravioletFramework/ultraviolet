59989a77ac52b5a28bcb32dfe2c2c6fbe94c8bf8
Cole Campbell <cole.campbell@twistedlogik.net>
2018-02-11 23:25:55 -0500

    Handle channel invalidation errors

---
+++ Source/Ultraviolet.FMOD/Shared/Native/FMOD_CHANNELCONTROL_TYPE.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ultraviolet.FMOD.Native
+{
+#pragma warning disable 1591
+    [Flags]
+    public enum FMOD_CHANNELCONTROL_TYPE : uint
+    {
+        FMOD_CHANNELCONTROL_CHANNEL,
+        FMOD_CHANNELCONTROL_CHANNELGROUP,
+    }
+#pragma warning restore 1591
+}
