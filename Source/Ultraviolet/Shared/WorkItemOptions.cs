4d52147291084d56b2393206962a6d9d1ec6a663
Cole Campbell <cole.campbell@twistedlogik.net>
2018-03-04 02:41:47 -0500

    Refactor QueueWorkItem() methods

---
+++ Source/Ultraviolet/Shared/WorkItemOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ultraviolet
+{
+    /// <summary>
+    /// Represents a set of values which are used to control the creation of Ultraviolet work items.
+    /// </summary>
+    [Flags]
+    public enum WorkItemOptions
+    {
+        /// <summary>
+        /// No options specified.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Indicates that the task should be forced to run asynchronously, even if it
+        /// was spawned on the Ultraviolet context thread.
+        /// </summary>
+        ForceAsynchronousExecution = 1,
+
+        /// <summary>
+        /// Indicates that the task returned can be <see langword="null"/> if the work
+        /// item was allowed to be executed synchronously.
+        /// </summary>
+        ReturnNullOnSynchronousExecution = 2,
+    }
+}
