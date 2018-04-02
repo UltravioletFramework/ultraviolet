e06e342dc743f6d1450dce635674845eb49410c5
Cole Campbell <cole.campbell@twistedlogik.net>
2018-04-01 21:05:17 -0400

    Allow FreeType2 fonts to have glyphs prepopulated when loaded

---
+++ Source/Ultraviolet.FreeType2/Shared/FreeTypeFontProcessorMetadata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Ultraviolet.FreeType2
+{
+    /// <summary>
+    /// Represents the asset metadata for the <see cref="FreeTypeFontProcessor"/> class.
+    /// </summary>
+    public sealed class FreeTypeFontProcessorMetadata
+    {
+        /// <summary>
+        /// Gets a string representing the list of glyphs which should be prepopulated on the font's texture atlas.
+        /// </summary>
+        [JsonProperty(PropertyName = "prepopulatedGlyphs")]
+        public String PrepopulatedGlyphs { get; private set; } = "ASCII";
+    }
+}
