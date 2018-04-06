2fa3fbac47f16612531b4d4106b7134fa2409756
Cole Campbell <cole.campbell@twistedlogik.net>
2018-04-06 16:51:06 -0400

    Add support for pixel sizing; clean up 32/64-bit code

--- Source/Ultraviolet.FreeType2/Shared/FreeTypeFontProcessorMetadata.cs
+++ Source/Ultraviolet.FreeType2/Shared/FreeTypeFontProcessorMetadata.cs
@@ -9,9 +9,20 @@ namespace Ultraviolet.FreeType2
     public sealed class FreeTypeFontProcessorMetadata
     {
         /// <summary>
-        /// Gets the size of the font in points.
+        /// Gets a value which specifies whether to use the nearest matching pixel size if <see cref="SizeInPixels"/> is specified
+        /// but the font face does not contain that pixel size.
         /// </summary>
-        public Single SizeInPoints { get; private set; } = 16f;
+        public Boolean UseClosestPixelSize { get; private set; } = false;
+
+        /// <summary>
+        /// Gets the size of the font in points. If <see cref="SizeInPixels"/> has a non-zero value, this value must be zero.
+        /// </summary>
+        public Int32 SizeInPoints { get; private set; } = 16;
+
+        /// <summary>
+        /// Gets the size of the font in pixels. If <see cref="SizeInPoints"/> has a non-zero value, this value must be zero.
+        /// </summary>
+        public Int32 SizeInPixels { get; private set; } = 0;
 
         /// <summary>
         /// Gets the font face's substitution character.
