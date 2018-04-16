eefd114ffb209cf789e68d61f742c05583d8def3
Cole Campbell <cole.campbell@twistedlogik.net>
2018-04-16 18:13:08 -0400

    Correctly handle UTF-8 text from SDL_GetClipboardText()

--- Source/Ultraviolet.SDL2/Shared/Platform/SDL2ClipboardService.cs
+++ Source/Ultraviolet.SDL2/Shared/Platform/SDL2ClipboardService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
 using Ultraviolet.Platform;
 using static Ultraviolet.SDL2.Native.SDLNative;
 
@@ -16,11 +18,25 @@ namespace Ultraviolet.SDL2.Platform
             {
                 if (SDL_HasClipboardText())
                 {
-                    var text = SDL_GetClipboardText();
-                    if (text == null)
+                    var ptr = SDL_GetClipboardText();
+                    if (ptr == IntPtr.Zero)
                         throw new SDL2Exception();
 
-                    return text;
+                    try
+                    {
+                        var length = 0;
+                        while (Marshal.ReadByte(ptr, length) != 0)
+                            length++;
+
+                        var utf8 = new Byte[length];
+                        Marshal.Copy(ptr, utf8, 0, utf8.Length);
+
+                        var utf16 = Encoding.Convert(Encoding.UTF8, Encoding.Unicode, utf8);
+                        var result = Encoding.Unicode.GetString(utf16);
+
+                        return result;
+                    }
+                    finally { SDL_free(ptr); }
                 }
                 return null;
             }
