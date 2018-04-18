3421750d128c1dee971082751926f3e660285eeb
Cole Campbell <cole.campbell@twistedlogik.net>
2018-04-18 09:33:49 -0400

    Add FreeType2.Native to pack script

--- Packages/pack.bat
+++ Packages/pack.bat
@@ -76,6 +76,10 @@ powershell -Command "(gc Ultraviolet.Presentation.nuspe_) -replace 'UV_VERSION',
 nuget pack Ultraviolet.Presentation.nuspec -Symbols
 @if %errorlevel% neq 0 @exit /b %errorlevel%
 
+powershell -Command "(gc Ultraviolet.FreeType2.Native.nuspe_) -replace 'UV_VERSION', '%UV_VERSION%' | sc Ultraviolet.FreeType2.Native.nuspec"
+nuget pack Ultraviolet.FreeType2.Native.nuspec -Symbols
+@if %errorlevel% neq 0 @exit /b %errorlevel%
+
 powershell -Command "(gc Ultraviolet.FreeType2.nuspe_) -replace 'UV_VERSION', '%UV_VERSION%' | sc Ultraviolet.FreeType2.nuspec"
 nuget pack Ultraviolet.FreeType2.nuspec -Symbols
 @if %errorlevel% neq 0 @exit /b %errorlevel%
