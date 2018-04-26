9a95516cffa7d86dbdebab214b626573cd83f915
Cole Campbell <cole.campbell@twistedlogik.net>
2018-04-26 11:32:20 -0400

    Update OpenGL factory init

---
+++ Source/Ultraviolet/Shared/Graphics/TextureOptions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ultraviolet.Graphics
+{
+    /// <summary>
+    /// Represents the set of options which can be used to modify the behavior of a <see cref="Texture2D"/> or <see cref="Texture3D"/>.
+    /// </summary>
+    [Flags]
+    public enum TextureOptions
+    {
+        /// <summary>
+        /// The render buffer has no special options.
+        /// </summary>
+        None = 0x00,
+
+        /// <summary>
+        /// Specifies that the render buffer should use immutable storage, if it is available.
+        /// </summary>
+        ImmutableStorage = 0x01,
+
+        /// <summary>
+        /// Specifies that the surface will contain linearly encoded color data.
+        /// Mutually exclusive with <see cref="SrgbColor"/>.
+        /// </summary>
+        LinearColor = 0x02,
+
+        /// <summary>
+        /// Specifies that the surface will contain sRGB encoded color data.
+        /// Mutually exclusive with <see cref="LinearColor"/>.
+        /// </summary>
+        SrgbColor = 0x04,
+
+        /// <summary>
+        /// The default options for a texture.
+        /// </summary>
+        Default = ImmutableStorage,
+    }
+}
