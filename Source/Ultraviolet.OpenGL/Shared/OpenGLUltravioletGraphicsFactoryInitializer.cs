cb36e2427be9fbab43ccd2fdad08b18bd171da67
Cole Campbell <cole.campbell@twistedlogik.net>
2018-04-26 11:32:20 -0400

    Update OpenGL factory init

--- Source/Ultraviolet.OpenGL/Shared/OpenGLUltravioletGraphicsFactoryInitializer.cs
+++ Source/Ultraviolet.OpenGL/Shared/OpenGLUltravioletGraphicsFactoryInitializer.cs
@@ -26,12 +26,12 @@ namespace Ultraviolet.OpenGL
             factory.SetFactoryMethod<DynamicIndexBufferFactory>((uv, itype, icount) => new OpenGLIndexBuffer(uv, itype, icount, gl.GL_DYNAMIC_DRAW));
             factory.SetFactoryMethod<Texture2DFromRawDataFactory>((uv, pixels, width, height, bytesPerPixel) => new OpenGLTexture2D(uv, pixels, width, height, bytesPerPixel));
             factory.SetFactoryMethod<Texture2DFactory>((uv, width, height, immutable) => new OpenGLTexture2D(uv, width, height, immutable));
-            factory.SetFactoryMethod<Texture3DFromRawDataFactory>((uv, layers, width, height, bytesPerPixel) => new OpenGLTexture3D(uv, layers, width, height, bytesPerPixel));
-            factory.SetFactoryMethod<Texture3DFactory>((uv, width, height, depth, immutable) => new OpenGLTexture3D(uv, width, height, depth, immutable));
+            factory.SetFactoryMethod<Texture3DFromRawDataFactory>((uv, layers, width, height, bytesPerPixel, srgb) => new OpenGLTexture3D(uv, layers, width, height, bytesPerPixel, srgb));
+            factory.SetFactoryMethod<Texture3DFactory>((uv, width, height, depth, options) => new OpenGLTexture3D(uv, width, height, depth, options));
             factory.SetFactoryMethod<RenderTarget2DFactory>((uv, width, height, usage) => new OpenGLRenderTarget2D(uv, width, height, usage));
             factory.SetFactoryMethod<RenderBuffer2DFactory>((uv, format, width, height, options) => new OpenGLRenderBuffer2D(uv, format, width, height, options));
             factory.SetFactoryMethod<DynamicTexture2DFactory>((uv, width, height, immutable, state, flushed) => new OpenGLDynamicTexture2D(uv, width, height, immutable, state, flushed));
-            factory.SetFactoryMethod<DynamicTexture3DFactory>((uv, width, height, depth, immutable, state, flushed) => new OpenGLDynamicTexture3D(uv, width, height, depth, immutable, state, flushed));
+            factory.SetFactoryMethod<DynamicTexture3DFactory>((uv, width, height, depth, options, state, flushed) => new OpenGLDynamicTexture3D(uv, width, height, depth, options, state, flushed));
 
             // Core effects
             factory.SetFactoryMethod<BasicEffectFactory>((uv) => new OpenGLBasicEffect(uv));
