4e6c91cc91b8ef2d0dc03201aa6660ee85b7a95f
Cole Campbell <cole.campbell@twistedlogik.net>
2018-04-26 09:45:01 -0400

    ADD sRGB encoding options to content metadata

---
+++ Source/Ultraviolet.SDL2/Shared/Graphics/SDL2Surface2DProcessorMetadata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Ultraviolet.Graphics;
+
+namespace Ultraviolet.SDL2.Graphics
+{
+    /// <summary>
+    /// Contains metadata for <see cref="SDL2Surface2DProcessor"/>.
+    /// </summary>
+    internal sealed class SDL2Surface2DProcessorMetadata
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the surface is SRGB encoded. If <see langword="null"/>, the
+        /// value specified by the <see cref="UltravioletContextProperties.SrgbDefaultForSurface2D"/> property is used.
+        /// </summary>
+        public Boolean? SrgbEncoded { get; set; }
+    }
+}
---
+++ Source/Ultraviolet.SDL2/Shared/Graphics/SDL2Surface3DProcessorMetadata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Ultraviolet.Graphics;
+
+namespace Ultraviolet.SDL2.Graphics
+{
+    /// <summary>
+    /// Contains metadata for <see cref="SDL2Surface3DProcessor"/>.
+    /// </summary>
+    internal sealed class SDL2Surface3DProcessorMetadata
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the surface is SRGB encoded. If <see langword="null"/>, the
+        /// value specified by the <see cref="UltravioletContextProperties.SrgbDefaultForSurface3D"/> property is used.
+        /// </summary>
+        public Boolean? SrgbEncoded { get; set; }
+    }
+}
