2668e4fb8b9a6a5dabf01ed6490608a2ab9e846c
Cole Campbell <cole.campbell@twistedlogik.net>
2018-05-07 21:57:29 -0400

    Remove Samples folder

--- Samples/Sample4_ContentManagement/Shared/Assets/GlobalTextureID.cs
+++
@@ -1,9 +0,0 @@
-﻿using Ultraviolet.Content;
-
-namespace UltravioletSample.Sample4_ContentManagement.Assets
-{
-    public static class GlobalTextureID
-    {
-        public static readonly AssetID Triangle;
-    }
-}
--- Samples/Sample5_RenderingSprites/Shared/Assets/GlobalSpriteID.cs
+++
@@ -1,9 +0,0 @@
-﻿using Ultraviolet.Content;
-
-namespace UltravioletSample.Sample5_RenderingSprites.Assets
-{
-    public static class GlobalSpriteID
-    {
-        public static readonly AssetID Explosion;
-    }
-}
