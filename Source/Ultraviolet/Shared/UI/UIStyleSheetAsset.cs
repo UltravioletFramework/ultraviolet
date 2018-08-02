ec6da5eeb5854dac837e2d6f63235139488caf97
Cole Campbell <cole.campbell@twistedlogik.net>
2018-08-02 18:37:11 -0400

    Add support for conditionally included style sheets

---
+++ Source/Ultraviolet/Shared/UI/UIStyleSheetAsset.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Ultraviolet.Core;
+
+namespace Ultraviolet.UI
+{
+    /// <summary>
+    /// Represents the source code for a particular style sheet.
+    /// </summary>
+    public sealed class UIStyleSheetAsset
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UIStyleSheetAsset"/> class.
+        /// </summary>
+        /// <param name="asset">The path to the asset which is dependent on the style sheet.</param>
+        public UIStyleSheetAsset(String asset)
+        {
+            Contract.Require(asset, nameof(asset));
+
+            this.Asset = asset;
+        }
+
+        /// <summary>
+        /// Gets the path to the asset which is dependent on the style sheet.
+        /// </summary>
+        public String Asset { get; }
+    }
+}
---
+++ Source/Ultraviolet/Shared/UI/UIStyleSheetSource.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ultraviolet.Core;
+
+namespace Ultraviolet.UI
+{
+    /// <summary>
+    /// Represents the source code for a particular style sheet.
+    /// </summary>
+    public sealed class UIStyleSheetSource
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UIStyleSheetSource"/> class.
+        /// </summary>
+        /// <param name="source">The style sheet source code.</param>
+        /// <param name="cultures">The list of cultures under which this style sheet is applied.</param>
+        /// <param name="languages">The list of languages under which this style sheet is applied.</param>
+        public UIStyleSheetSource(String source, IEnumerable<String> cultures = null, IEnumerable<String> languages = null)
+        {
+            Contract.Require(source, nameof(source));
+
+            this.Source = source;
+            this.Cultures = new List<String>(cultures ?? Enumerable.Empty<String>());
+            this.Languages = new List<String>(languages ?? Enumerable.Empty<String>());
+        }
+
+        /// <summary>
+        /// Gets the style sheet source code.
+        /// </summary>
+        public String Source { get; }
+
+        /// <summary>
+        /// Gets the culture for which this source is applied.
+        /// </summary>
+        public IReadOnlyList<String> Cultures { get; }
+
+        /// <summary>
+        /// Gets the language for which this source is applied.
+        /// </summary>
+        public IReadOnlyList<String> Languages { get; }
+    }
+}
