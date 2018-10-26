7a7de0bc79c45fe20a7dce6a7b4706ccbaee3c54
Cole Campbell <cole.campbell@twistedlogik.net>
2018-10-26 16:06:21 -0400

    Create ImGui bindings library

---
+++ Source/Ultraviolet.ImGuiViewProvider.Bindings/Desktop/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(false)]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(@"Ultraviolet ImGui Bindings Library")]
+[assembly: AssemblyDescription(
+    @"Contains the Ultraviolet Framework's C# bindings to the Dear ImGui API.")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("4f421cdb-e9d0-485f-86b3-ff57b4f1e459")]
