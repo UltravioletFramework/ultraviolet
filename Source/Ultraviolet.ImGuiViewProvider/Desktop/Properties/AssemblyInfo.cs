a802250d8604fcf961eccb393b82b11829276eb9
Cole Campbell <cole.campbell@twistedlogik.net>
2018-10-26 15:41:06 -0400

    Add Signed configurations to ImGui

--- Source/Ultraviolet.ImGuiViewProvider/Desktop/Properties/AssemblyInfo.cs
+++ Source/Ultraviolet.ImGuiViewProvider/Desktop/Properties/AssemblyInfo.cs
@@ -1,36 +1,23 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+[assembly: CLSCompliant(false)]
+
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ultraviolet.ImGuiViewProvider.Desktop")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ultraviolet.ImGuiViewProvider.Desktop")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle(@"Ultraviolet ImGui View Provider Plugin")]
+[assembly: AssemblyDescription(
+    @"A plugin for the Ultraviolet Framework which allows user interfaces to be built using the Dear ImGui library.")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
 [assembly: Guid("b4f47385-b462-4826-809e-117603f24cf9")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
