d2b94539e8844a98099fafa1719b3ef84a9338c3
Cole Campbell <cole.campbell@twistedlogik.net>
2018-12-01 13:59:24 -0500

    Add macOSModern shim

---
+++ Source/Ultraviolet.Shims.macOSModern/macOSModern/Platform/macOSModernIconLoader.cs
@@ -0,0 +1,19 @@
+﻿using Ultraviolet.Graphics;
+using Ultraviolet.Platform;
+
+namespace Ultraviolet.Shims.macOSModern.Platform
+{
+    /// <summary>
+    /// Represents an implementation of the <see cref="IconLoader"/> class for macOS.
+    /// </summary>
+    public sealed class macOSModernIconLoader : IconLoader
+    {
+        /// <inheritdoc/>
+        public override Surface2D LoadIcon()
+        {
+            /* Application icon should be handled in Info.plist */
+            return null;
+        }
+    }
+}
+
