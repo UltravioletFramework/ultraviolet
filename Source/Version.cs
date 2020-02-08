17706a99c0901dc200489eddcff04294b8b7db77
Cole Campbell <cole.campbell@twistedlogik.net>
2020-02-08 18:00:50 -0500

    Update default version info

--- Source/Version.cs
+++ Source/Version.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("Ultraviolet Framework")]
-[assembly: AssemblyCopyright("Copyright (c) Cole Campbell 2014-2017")]
+[assembly: AssemblyCopyright("Copyright (c) Cole Campbell 2014-2020")]
 
-[assembly: AssemblyVersion("2017.12.0.0")]
-[assembly: AssemblyFileVersion("2017.12.0.0")]
-[assembly: AssemblyInformationalVersion("2017.12.0.0 dev build")]
+[assembly: AssemblyVersion("2020.2.0.0")]
+[assembly: AssemblyFileVersion("2020.2.0.0")]
+[assembly: AssemblyInformationalVersion("2020.2.0.0 dev build")]
