231ec48e84f2e918d9858ab0ef2e93237ad30a18
Cole Campbell <cole.campbell@twistedlogik.net>
2020-04-05 17:40:36 -0400

    Generate BASSFX native interop code

---
+++ Source/Ultraviolet.BASS/Shared/Native/BASSFXNativeImpl_iOS.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Security;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Ultraviolet.BASS.Native
+{
+#pragma warning disable 1591
+    [SuppressUnmanagedCodeSecurity]
+    internal sealed unsafe class BASSFXNativeImpl_iOS : BASSFXNativeImpl
+    {
+        [DllImport("__Internal", EntryPoint = "BASS_FX_TempoCreate", CallingConvention = CallingConvention.StdCall)]
+        private static extern UInt32 INTERNAL_BASS_FX_TempoCreate(UInt32 chan, UInt32 flags);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override sealed UInt32 BASS_FX_TempoCreate(UInt32 chan, UInt32 flags) => INTERNAL_BASS_FX_TempoCreate(chan, flags);
+    }
+#pragma warning restore 1591
+}
