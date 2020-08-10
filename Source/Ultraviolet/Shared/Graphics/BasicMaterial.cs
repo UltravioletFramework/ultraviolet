60e75511d732a1d4876645d8964c835d2c655f25
Cole Campbell <cole.campbell@twistedlogik.net>
2020-08-09 22:36:57 -0400

    Implement BasicMaterial

---
+++ Source/Ultraviolet/Shared/Graphics/BasicMaterial.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Ultraviolet.Graphics
+{
+    /// <summary>
+    /// Represents a <see cref="Material"/> which uses <see cref="BasicEffect"/> to shade meshes.
+    /// </summary>
+    public class BasicMaterial : Material
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BasicMaterial"/> class.
+        /// </summary>
+        /// <param name="effectInstance">The <see cref="BasicEffect"/> instance which this material uses when
+        /// drawing meshes, or <see langword="null"/> to use a shared effect instance.</param>
+        public BasicMaterial(BasicEffect effectInstance = null)
+            : base(effectInstance ?? SharedEffect)
+        {
+
+        }
+
+        /// <summary>
+        /// Gets the shared <see cref="BasicEffect"/> instance which <see cref="BasicMaterial"/> instances use by default when drawing meshes.
+        /// </summary>
+        public static BasicEffect SharedEffect => sharedEffectSingleton.Value;
+
+        /// <inheritdoc/>
+        public override void Apply()
+        {
+            Effect.DiffuseColor = DiffuseColor;
+            Effect.EmissiveColor = EmissiveColor;
+            Effect.SpecularColor = SpecularColor;
+            Effect.SpecularPower = SpecularPower;
+            Effect.Texture = Texture;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="BasicEffect"/> instance which this material uses when drawing meshes.
+        /// </summary>
+        public new BasicEffect Effect => (BasicEffect)base.Effect;
+
+        /// <summary>
+        /// Gets or sets the material's diffuse color.
+        /// </summary>
+        public Color DiffuseColor { get; set; } = Color.White;
+
+        /// <summary>
+        /// Gets or sets the material's emissive color.
+        /// </summary>
+        public Color EmissiveColor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the material's specular color.
+        /// </summary>
+        public Color SpecularColor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the material's specular power.
+        /// </summary>
+        public Single SpecularPower { get; set; } = 16f;
+        
+        /// <summary>
+        /// Gets or sets the material's texture.
+        /// </summary>
+        public Texture2D Texture { get; set; }
+
+        // Shared effect.
+        private static readonly UltravioletSingleton<BasicEffect> sharedEffectSingleton =
+            new UltravioletSingleton<BasicEffect>(UltravioletSingletonFlags.DisabledInServiceMode | UltravioletSingletonFlags.Lazy,
+                uv => BasicEffect.Create());
+    }
+}
---
+++ Source/Ultraviolet/Shared/Graphics/Material.cs
@@ -0,0 +1,31 @@
+﻿using Ultraviolet.Core;
+
+namespace Ultraviolet.Graphics
+{
+    /// <summary>
+    /// Represents the material properties of a mesh.
+    /// </summary>
+    public abstract class Material
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Material"/> class.
+        /// </summary>
+        /// <param name="effectInstance">The material's <see cref="Effect"/> instance.</param>
+        public Material(Effect effectInstance)
+        {
+            Contract.Require(effectInstance, nameof(effectInstance));
+
+            this.Effect = effectInstance;
+        }
+
+        /// <summary>
+        /// Applies the material for rendering.
+        /// </summary>
+        public abstract void Apply();
+
+        /// <summary>
+        /// Gets the <see cref="Effect"/> in
+        /// </summary>
+        public Effect Effect { get; }
+    }
+}
