c7d8bdb15accc0d42cb8330af1ac964e3ab732d5
Cole Campbell <cole.campbell@twistedlogik.net>
2020-09-26 15:17:32 -0400

    Make Matrix properties settable

--- Source/Ultraviolet/Shared/Matrix.cs
+++ Source/Ultraviolet/Shared/Matrix.cs
@@ -2299,37 +2299,100 @@ namespace Ultraviolet
         /// <summary>
         /// Gets the matrix's right vector.
         /// </summary>
-        public Vector3 Right { get { return new Vector3(M11, M12, M13); } }
+        public Vector3 Right 
+        { 
+            get { return new Vector3(M11, M12, M13); }
+            set
+            {
+                M11 = value.X;
+                M12 = value.Y;
+                M13 = value.Z;
+            }
+        }
 
         /// <summary>
         /// Gets the matrix's left vector.
         /// </summary>
-        public Vector3 Left { get { return new Vector3(-M11, -M12, -M13); } }
+        public Vector3 Left
+        {
+            get { return new Vector3(-M11, -M12, -M13); }
+            set
+            {
+                M11 = -value.X;
+                M12 = -value.Y;
+                M13 = -value.Z;
+            }
+        }
 
         /// <summary>
         /// Gets the matrix's up vector.
         /// </summary>
-        public Vector3 Up { get { return new Vector3(M21, M22, M23); } }
+        public Vector3 Up 
+        { 
+            get { return new Vector3(M21, M22, M23); }
+            set
+            {
+                M21 = value.X;
+                M22 = value.Y;
+                M23 = value.Z;
+            }
+        }
 
         /// <summary>
         /// Gets the matrix's down vector.
         /// </summary>
-        public Vector3 Down { get { return new Vector3(-M21, -M22, -M23); } }
+        public Vector3 Down 
+        { 
+            get { return new Vector3(-M21, -M22, -M23); }
+            set
+            {
+                M21 = -value.X;
+                M22 = -value.Y;
+                M23 = -value.Z;
+            }
+        }
 
         /// <summary>
         /// Gets the matrix's backwards vector.
         /// </summary>
-        public Vector3 Backward { get { return new Vector3(M31, M32, M33); } }
+        public Vector3 Backward 
+        { 
+            get { return new Vector3(M31, M32, M33); }
+            set
+            {
+                M31 = value.X;
+                M32 = value.Y;
+                M33 = value.Z;
+            }
+        }
 
         /// <summary>
         /// Gets the matrix's forwards vector.
         /// </summary>
-        public Vector3 Forward { get { return new Vector3(-M31, -M32, -M33); } }
+        public Vector3 Forward
+        { 
+            get { return new Vector3(-M31, -M32, -M33); }
+            set
+            {
+                M31 = -value.X;
+                M32 = -value.Y;
+                M33 = -value.Z;
+            }
+        }
 
         /// <summary>
         /// Gets the matrix's translation vector.
         /// </summary>
-        public Vector3 Translation { get { return new Vector3(M41, M42, M43); } }
+        public Vector3 Translation 
+        { 
+            get { return new Vector3(M41, M42, M43); }
+            set
+            {
+                M41 = value.X;
+                M42 = value.Y;
+                M43 = value.Z;
+            }
+        }
 
         /// <summary>
         /// Gets the value at row 1, column 1 of the matrix.
