4e7ea8fe038dd26ca5f367dffbf724878c40c915
Cole Campbell <cole.campbell@twistedlogik.net>
2020-09-29 00:18:31 -0400

    Add bone matrix tests

---
+++ Source/Ultraviolet.Tests/Shared/Graphics/Graphics3D/SkinnedModelBoneData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ultraviolet.Tests.Graphics.Graphics3D
+{
+    /// <summary>
+    /// Represents the expected data for a skinned model's animated bones.
+    /// </summary>
+    public class SkinnedModelBoneData
+    {
+        public Double Time { get; set; }
+        public Int32 Bone { get; set; }
+        public Single M11 { get; set; }
+        public Single M12 { get; set; }
+        public Single M13 { get; set; }
+        public Single M14 { get; set; }
+        public Single M21 { get; set; }
+        public Single M22 { get; set; }
+        public Single M23 { get; set; }
+        public Single M24 { get; set; }
+        public Single M31 { get; set; }
+        public Single M32 { get; set; }
+        public Single M33 { get; set; }
+        public Single M34 { get; set; }
+        public Single M41 { get; set; }
+        public Single M42 { get; set; }
+        public Single M43 { get; set; }
+        public Single M44 { get; set; }
+    }
+}
---
+++ Source/Ultraviolet.Tests/Shared/Graphics/Graphics3D/SkinnedModelInstanceTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using NUnit.Framework;
+using Ultraviolet.Graphics.Graphics3D;
+using Ultraviolet.TestApplication;
+
+namespace Ultraviolet.Tests.Graphics.Graphics3D
+{
+    [TestFixture]
+    public class SkinnedModelInstanceTests : UltravioletApplicationTestFramework
+    {
+        private Dictionary<Double, Matrix[]> LoadBoneData(String name)
+        {
+            using (var stream = File.OpenRead(Path.Combine("Resources", "Expected", "Graphics", "Graphics3D", name)))
+            using (var reader = new StreamReader(stream))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Read();
+                csv.ReadHeader();
+
+                var records = csv.GetRecords<SkinnedModelBoneData>().ToArray();
+                var result = records.GroupBy(x => x.Time).ToDictionary(x => x.Key, records =>
+                {
+                    var bones = new Matrix[records.Count()];
+
+                    foreach (var record in records)
+                    {
+                        bones[record.Bone] = new Matrix(
+                            record.M11, record.M12, record.M13, record.M14,
+                            record.M21, record.M22, record.M23, record.M24,
+                            record.M31, record.M32, record.M33, record.M34,
+                            record.M41, record.M42, record.M43, record.M44);
+                    }
+
+                    return bones;
+                });
+
+                return result;
+            }
+        }
+
+        [Test]
+        [TestCase("Models/SimpleSkin", "BoneMatrices_SimpleSkin.csv")]
+        [TestCase("Models/CesiumMan", "BoneMatrices_CesiumMan.csv")]
+        [Category("Content")]
+        public void SkinnedModelInstance_CalculatesCorrectBoneMatrices(String modelAssetFile, String modelBoneDataFile)
+        {
+            GivenAnUltravioletApplicationWithNoWindow()
+                .WithContent(content =>
+                {
+                    var expectedByTime = LoadBoneData(modelBoneDataFile);
+
+                    var model = content.Load<SkinnedModel>(modelAssetFile);
+                    var modelInstance = new SkinnedModelInstance(model);
+
+                    var animationController = modelInstance.PlayAnimation(SkinnedAnimationMode.Manual, 0);
+                    var animationDuration = (Int32)Math.Ceiling(model.Animations[0].Duration);
+                    for (var t = 0.0; t < animationDuration; t += 0.25)
+                    {
+                        animationController.SetPosition(t % animationController.Duration);
+
+                        var skin = animationController.Model.Skins[0];
+                        skin.Update();
+
+                        var boneCount = skin.BoneCount;
+                        var boneTransforms = skin.GetBoneTransforms();
+                        var boneTransformsExpected = expectedByTime[t];
+
+                        TheResultingValue(boneTransformsExpected.Length)
+                            .ShouldBe(boneTransforms.Length);
+    
+                        for (var i = 0; i < boneTransforms.Length; i++)
+                        {
+                            TheResultingValue(boneTransformsExpected[i])
+                                .WithinDelta(0.0001f).ShouldBe(boneTransforms[i]);
+                        }
+                    }
+                })
+                .RunForOneFrame();
+        }
+    }
+}
